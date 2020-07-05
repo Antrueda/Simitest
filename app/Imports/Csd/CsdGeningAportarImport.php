95da79e852756763f03496bbe92659e64e9937cd
Antrueda <63014201+Antrueda@users.noreply.github.com>
2020-07-05 18:14:49 -0500

    Add files via upload

--- app/Imports/Csd/CsdGeningAportarImport.php
+++ app/Imports/Csd/CsdGeningAportarImport.php
@@ -16,7 +16,17 @@ class CsdGeningAportarImport implements ToModel
     public function model(array $row)
     {
         return new ConsultaCsdGeningAporta([
-            //
-        ]);
+            'csd_id'=> $row[0],
+            'prm_aporta_id'=> $row[1],
+            'mensual'=> $row[2],
+            'aporte'=> $row[3],
+            'jornada_entre'=> $row[4],
+            'prm_entre_id'=> $row[5],
+            'jornada_a'=> $row[6],
+            'prm_a_id'=> $row[7],
+            'user_crea_id'=> 1,
+            'user_edita_id'=> 1,
+            'sis_esta_id'=> 1
+            ]);
+        }
     }
-}
