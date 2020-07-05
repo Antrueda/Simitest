c1aad765d9d8490ed03873b3364f179c32d0e82a
Antrueda <63014201+Antrueda@users.noreply.github.com>
2020-07-05 16:30:21 -0500

    Add files via upload

---
+++ database/seeds/CsdBienvenidaSeeder.php
@@ -0,0 +1,132 @@
+<?php
+
+use App\Models\consulta\CsdBienvenida;
+use Illuminate\Database\Seeder;
+
+class CsdBienvenidaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        CsdBienvenida::create(['id'=>1,'csd_id'=>1, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>2,'csd_id'=>1, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>3,'csd_id'=>1, 'prm_persona_id'=>769, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>4,'csd_id'=>2, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>5,'csd_id'=>2, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>6,'csd_id'=>3, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>7,'csd_id'=>3, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>8,'csd_id'=>4, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>9,'csd_id'=>4, 'prm_persona_id'=>762, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>10,'csd_id'=>4, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>11,'csd_id'=>5, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>12,'csd_id'=>5, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>13,'csd_id'=>6, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>14,'csd_id'=>7, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>15,'csd_id'=>8, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>16,'csd_id'=>9, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>17,'csd_id'=>10, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>18,'csd_id'=>11, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>19,'csd_id'=>12, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>20,'csd_id'=>14, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>21,'csd_id'=>15, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>22,'csd_id'=>16, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>23,'csd_id'=>17, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>24,'csd_id'=>17, 'prm_persona_id'=>566, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>25,'csd_id'=>17, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>26,'csd_id'=>24, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>27,'csd_id'=>24, 'prm_persona_id'=>762, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>28,'csd_id'=>25, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>29,'csd_id'=>25, 'prm_persona_id'=>764, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>30,'csd_id'=>26, 'prm_persona_id'=>762, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>31,'csd_id'=>27, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>32,'csd_id'=>28, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>33,'csd_id'=>29, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>34,'csd_id'=>29, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>35,'csd_id'=>29, 'prm_persona_id'=>766, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>36,'csd_id'=>30, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>37,'csd_id'=>31, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>38,'csd_id'=>32, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>39,'csd_id'=>33, 'prm_persona_id'=>762, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>40,'csd_id'=>34, 'prm_persona_id'=>766, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>41,'csd_id'=>35, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>42,'csd_id'=>36, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>43,'csd_id'=>37, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>44,'csd_id'=>38, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>45,'csd_id'=>39, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>46,'csd_id'=>40, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>47,'csd_id'=>14, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>48,'csd_id'=>42, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>49,'csd_id'=>43, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>50,'csd_id'=>44, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>51,'csd_id'=>45, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>52,'csd_id'=>46, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>53,'csd_id'=>47, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>54,'csd_id'=>48, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>55,'csd_id'=>49, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>56,'csd_id'=>50, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>57,'csd_id'=>51, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>58,'csd_id'=>16, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>59,'csd_id'=>53, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>60,'csd_id'=>53, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>61,'csd_id'=>54, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>62,'csd_id'=>54, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>63,'csd_id'=>55, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>64,'csd_id'=>55, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>65,'csd_id'=>56, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>66,'csd_id'=>56, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>67,'csd_id'=>57, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>68,'csd_id'=>57, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>69,'csd_id'=>58, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>70,'csd_id'=>59, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>71,'csd_id'=>60, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>72,'csd_id'=>60, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>73,'csd_id'=>60, 'prm_persona_id'=>766, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>74,'csd_id'=>61, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>75,'csd_id'=>61, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>76,'csd_id'=>62, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>77,'csd_id'=>62, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>78,'csd_id'=>63, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>79,'csd_id'=>63, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>80,'csd_id'=>64, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>81,'csd_id'=>65, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>82,'csd_id'=>66, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>83,'csd_id'=>67, 'prm_persona_id'=>766, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>84,'csd_id'=>67, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>85,'csd_id'=>68, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>86,'csd_id'=>68, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>87,'csd_id'=>69, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>88,'csd_id'=>70, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>89,'csd_id'=>71, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>90,'csd_id'=>72, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>91,'csd_id'=>73, 'prm_persona_id'=>807, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>92,'csd_id'=>74, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>93,'csd_id'=>75, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>94,'csd_id'=>76, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>95,'csd_id'=>77, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>96,'csd_id'=>78, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>97,'csd_id'=>78, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>98,'csd_id'=>34, 'prm_persona_id'=>766, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>99,'csd_id'=>80, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>100,'csd_id'=>81, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>101,'csd_id'=>83, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>102,'csd_id'=>84, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>103,'csd_id'=>54, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>104,'csd_id'=>54, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>105,'csd_id'=>86, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>106,'csd_id'=>87, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>107,'csd_id'=>88, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>108,'csd_id'=>89, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>109,'csd_id'=>89, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>110,'csd_id'=>89, 'prm_persona_id'=>769, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>111,'csd_id'=>90, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>112,'csd_id'=>91, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>113,'csd_id'=>91, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>114,'csd_id'=>92, 'prm_persona_id'=>761, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        CsdBienvenida::create(['id'=>115,'csd_id'=>92, 'prm_persona_id'=>765, 'user_crea_id'=>1,'user_edita_id'=>1,'sis_esta_id'=>1]);
+        
+    }
+}
