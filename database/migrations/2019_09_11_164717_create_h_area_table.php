22ed810539c91938663c81125605d97a9a114a2e
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-07-30 11:37:35 -0500

    Parametro nuevo

---
+++ database/migrations/2019_09_11_164717_create_h_area_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateHAreaTable extends Migration
+{
+    private $tablaxxx = 'h_areas';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tablaxxx, function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 120)->unique();
+            $table->string('contexto', 2)->nullable();
+            $table->text('descripcion', 4000)->nullable();
+            $table->bigInteger('sis_esta_id')->unsigned()->default(1);
+            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
+            $table->bigInteger('user_crea_id')->unsigned();
+            $table->bigInteger('user_edita_id')->unsigned();            
+            $table->timestamps();
+        });
+        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS LOGS DE LA TABLA {$this->tablaxxx}'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablaxxx);
+    }
+}
