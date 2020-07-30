127373970d4567f26762758b0e421886aa3935da
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-07-30 13:39:14 -0500

    Avance Documentar tablas y crear tabla LOG h_fi_nucleo_familiars

--- database/migrations/2019_09_11_174108_create_fi_nucleo_familiars_table.php
+++ database/migrations/2019_09_11_174108_create_fi_nucleo_familiars_table.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateFiNucleoFamiliarsTable extends Migration
 {
+    private $tablaxxx = 'fi_nucleo_familiars';
     /**
      * Run the migrations.
      *
@@ -13,18 +15,18 @@ class CreateFiNucleoFamiliarsTable extends Migration
      */
     public function up()
     {
-        Schema::create('fi_nucleo_familiars', function (Blueprint $table) {
-            
+        Schema::create($this->tablaxxx, function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('i_en_uso');
-            $table->bigInteger('user_crea_id')->unsigned(); 
+            $table->bigInteger('user_crea_id')->unsigned();
             $table->bigInteger('user_edita_id')->unsigned();
             $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-      $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
+            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
             $table->timestamps();
             $table->foreign('user_crea_id')->references('id')->on('users');
             $table->foreign('user_edita_id')->references('id')->on('users');
         });
+        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA EL LISTADO QUE DESCRIBE EL NÚCLEO FAMILIAR DE UN NNAJ.'");
     }
 
     /**
@@ -34,6 +36,6 @@ class CreateFiNucleoFamiliarsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fi_nucleo_familiars');
+        Schema::dropIfExists($this->tablaxxx);
     }
 }
---
+++ database/migrations/2019_09_11_174108_create_h_fi_nucleo_familiars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateHFiNucleoFamiliarsTable extends Migration
+{
+    private $tablaxxx = 'h_fi_nucleo_familiars';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tablaxxx, function (Blueprint $table) {
+            $table->id();
+            $table->bigIncrements('id');
+            $table->integer('i_en_uso');
+            $table->bigInteger('user_crea_id')->unsigned();
+            $table->bigInteger('user_edita_id')->unsigned();
+            $table->bigInteger('sis_esta_id')->unsigned()->default(1);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('h_fi_nucleo_familiars');
+    }
+}
