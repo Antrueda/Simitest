27534ea7ab952358b2332cdda16a5bf4c7270b8c
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-07-31 15:58:38 -0500

    Avance en documentar tabla y crear tabla LOG de vsi_saluds

--- database/migrations/2019_09_25_202223_create_h_vsi_saluds_table.php
+++ database/migrations/2019_09_25_202223_create_h_vsi_saluds_table.php
@@ -5,9 +5,9 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateVsiSaludsTable extends Migration
+class CreateHVsiSaludsTable extends Migration
 {
-    private $tablaxxx = 'vsi_saluds';
+    private $tablaxxx = 'h_vsi_saluds';
     /**
      * Run the migrations.
      *
@@ -36,23 +36,9 @@ class CreateVsiSaludsTable extends Migration
             $table->bigInteger('user_crea_id')->unsigned();
             $table->bigInteger('user_edita_id')->unsigned();
             $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
             $table->timestamps();
-
-            $table->foreign('vsi_id')->references('id')->on('vsis');
-            $table->foreign('prm_atencion_id')->references('id')->on('parametros');
-            $table->foreign('prm_condicion_id')->references('id')->on('parametros');
-            $table->foreign('prm_medicamento_id')->references('id')->on('parametros');
-            $table->foreign('prm_prescripcion_id')->references('id')->on('parametros');
-            $table->foreign('prm_sexual_id')->references('id')->on('parametros');
-            $table->foreign('prm_activa_id')->references('id')->on('parametros');
-            $table->foreign('prm_embarazo_id')->references('id')->on('parametros');
-            $table->foreign('prm_hijo_id')->references('id')->on('parametros');
-            $table->foreign('prm_interrupcion_id')->references('id')->on('parametros');
-            $table->foreign('user_crea_id')->references('id')->on('users');
-            $table->foreign('user_edita_id')->references('id')->on('users');
         });
-        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS DETALLES DE LA SALUD DEL NNAJ.'");
+        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS LOGS DE LA TABLA {$this->tablaxxx}'");
     }
 
     /**
