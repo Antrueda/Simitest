7c80cc4371ce83d03dfc50610ce43ccbfb80917d
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-07-31 12:40:12 -0500

    Avance en documentar tabla y crear tabla LOG en sis_actividad_procesos

--- database/migrations/2020_07_31_122438_create_h_sis_actividad_procesos_table.php
+++ database/migrations/2020_07_31_122438_create_h_sis_actividad_procesos_table.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateSisActividadProcesosTable extends Migration
+class CreateHSisActividadProcesosTable extends Migration
 {
-    private $tablaxxx = 'sis_actividad_procesos';
+    private $tablaxxx = 'h_sis_actividad_procesos';
     /**
      * Run the migrations.
      *
@@ -23,15 +23,9 @@ class CreateSisActividadProcesosTable extends Migration
             $table->bigInteger('user_crea_id')->unsigned(); 
             $table->bigInteger('user_edita_id')->unsigned();
             $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
             $table->timestamps();
-
-            $table->foreign('sis_actividad_id')->references('id')->on('sis_actividads');
-            $table->foreign('sis_proceso_id')->references('id')->on('sis_procesos');
-            $table->foreign('user_crea_id')->references('id')->on('users');
-            $table->foreign('user_edita_id')->references('id')->on('users');
         });
-        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LA RELACIÓN ENTRE LAS ACTIVIDADES Y LOS PROCESOS DEL SISTEMA'");
+        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS LOGS DE LA TABLA {$this->tablaxxx}'");
     }
 
     /**
