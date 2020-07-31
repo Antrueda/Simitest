41fc8007e62be138ce1fae3e638da73ca4f285fd
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-07-31 14:02:00 -0500

    Avance en documentar tabla y crear tabla LOG en sis_institucion_edus.

--- database/migrations/2019_09_25_162650_create_h_sis_institucion_edus_table.php
+++ database/migrations/2019_09_25_162650_create_h_sis_institucion_edus_table.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateSisInstitucionEdusTable extends Migration
+class CreateHSisInstitucionEdusTable extends Migration
 {
-    private $tablaxxx = 'sis_institucion_edus';
+    private $tablaxxx = 'h_sis_institucion_edus';
     /**
      * Run the migrations.
      *
@@ -29,20 +29,9 @@ class CreateSisInstitucionEdusTable extends Migration
             $table->bigInteger('i_usr_coord_academico_id')->unsigned();
             $table->bigInteger('user_edita_id')->unsigned();
             $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
             $table->timestamps();
-
-            $table->foreign('user_crea_id')->references('id')->on('users');
-            $table->foreign('user_edita_id')->references('id')->on('users');
-
-            $table->foreign('sis_municipio_id')->references('id')->on('sis_municipios');
-            $table->foreign('sis_departamento_id')->references('id')->on('sis_departamentos');
-            $table->foreign('i_prm_sector_id')->references('id')->on('parametros');
-            $table->foreign('i_usr_rector_id')->references('id')->on('users');
-            $table->foreign('i_usr_secretario_id')->references('id')->on('users');
-            $table->foreign('i_usr_coord_academico_id')->references('id')->on('users');
         });
-        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS DATOS BASICOS DE IDENTIFICACIÓN, UBICACIÓN Y DE CONTACTO DE CENTROS EDUCATIVOS'");
+        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS LOGS DE LA TABLA {$this->tablaxxx}'");
     }
 
     /**
