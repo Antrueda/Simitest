ec4a81ceb432e8095148d79774b407a95d88b849
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-07-31 13:15:35 -0500

    Avance en documentar tabla y crear tabla LOG en fi_contactos

--- database/migrations/2019_09_25_162649_create_h_fi_contactos_table.php
+++ database/migrations/2019_09_25_162649_create_h_fi_contactos_table.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateFiContactosTable extends Migration
+class CreateHFiContactosTable extends Migration
 {
-    private $tablaxxx = 'fi_contactos';
+    private $tablaxxx = 'h_fi_contactos';
     /**
      * Run the migrations.
      *
@@ -24,23 +24,14 @@ class CreateFiContactosTable extends Migration
             $table->date('d_fecha_remite_id')->nullable();
             $table->bigInteger('i_prm_motivo_contacto_id')->nullable()->unsigned();
             $table->bigInteger('i_prm_aut_tratamiento_id')->unsigned();
-            
+
             $table->bigInteger('sis_nnaj_id')->unsigned();
-            $table->bigInteger('user_crea_id')->unsigned(); 
+            $table->bigInteger('user_crea_id')->unsigned();
             $table->bigInteger('user_edita_id')->unsigned();
             $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-      $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
             $table->timestamps();
-            $table->foreign('user_crea_id')->references('id')->on('users');
-            $table->foreign('user_edita_id')->references('id')->on('users');
-            $table->foreign('sis_nnaj_id')->references('id')->on('sis_nnajs');
-            $table->foreign('i_prm_tipo_contacto_id')->references('id')->on('parametros');
-            $table->foreign('i_prm_contacto_opcion_id')->references('id')->on('parametros');
-            $table->foreign('i_prm_motivo_contacto_id')->references('id')->on('parametros');
-            $table->foreign('i_prm_aut_tratamiento_id')->references('id')->on('parametros');
-            
         });
-        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS DATOS DE CONTACTO DE LAS PERSONAS QUE SE PUEDEN CONTACTAR CON LOS NNAJ'");
+        DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LOS LOGS DE LA TABLA {$this->tablaxxx}'");
     }
 
     /**
