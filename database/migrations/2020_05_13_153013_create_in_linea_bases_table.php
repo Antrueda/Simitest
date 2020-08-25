7428941e64d1a103cd94b7e86d8a3c1af329e374
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-08-25 12:32:23 -0500

    Verificación y correccion de los archivos ascociados a in_linea_bases

--- database/migrations/2020_05_13_153013_create_in_linea_bases_table.php
+++ database/migrations/2020_05_13_153013_create_in_linea_bases_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CamposMagicos\CamposMagicos;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,14 +19,7 @@ class CreateInLineaBasesTable extends Migration
         Schema::create($this->tablaxxx, function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('s_linea_base', 300)->unique();
-            $table->bigInteger('user_crea_id')->unsigned();
-            $table->bigInteger('user_edita_id')->unsigned();
-            $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-
-            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
-            $table->foreign('user_crea_id')->references('id')->on('users');
-            $table->foreign('user_edita_id')->references('id')->on('users');
-            $table->timestamps();
+            $table = CamposMagicos::magicos($table);
         });
         DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA EL LISTADO DE LAS DESCRIPCIONES DE LA LINEA DE BASE'");
     }
--- database/seeds/InLineaBasesSeeder.php
+++ database/seeds/InLineaBasesSeeder.php
@@ -12,33 +12,33 @@ class InLineaBasesSeeder extends Seeder
    */
   public function run()
   {
-    InLineaBase::create(['s_linea_base' => 'El NNAJ presenta dificultad para interactuar con las demás personas en el #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'El NNAJ presenta #######1 para relacionarse con los demás.',]);
-    InLineaBase::create(['s_linea_base' => 'El NNAJ reacciona de manera #######1 ante eventos o situaciones que generan cambio emocional.',]);
-    InLineaBase::create(['s_linea_base' => 'El NNAJ no cuentan con redes sociales de apoyo.',]);
-    InLineaBase::create(['s_linea_base' => 'La familia del NNAJ no cuenta con redes sociales de apoyo.',]);
-    InLineaBase::create(['s_linea_base' => '#######1 presenta dificultades de acceso a los servicios brindados por las redes sociales de apoyo.',]);
-    InLineaBase::create(['s_linea_base' => 'La red de apoyo social representa factores de riesgo para el NNAJ y/o su familia.',]);
-    InLineaBase::create(['s_linea_base' => 'El NNA ha sido victima de la ESCNNA #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de ESCNNA en la modalidad de ########',]);
-    InLineaBase::create(['s_linea_base' => 'El NNA ha tenido #######1 de quitarse la vida #######2 por eventos relacionados con su situación como victima de la ESCNNA.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de factores de riesgo de ESCNNA #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de factores de riesgo de ESCNNA naturalizados por parte de la familia como parte de su situación cotidiana.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de presunto abuso sexual #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de evento traumático ante presunto abuso sexual, sin apoyo #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de evento traumático ante presunto abuso sexual #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de #######1, Ante el evento el NNAJ ha emitido #######2 de quitarse la vida.',]);
-    InLineaBase::create(['s_linea_base' => 'El AJ ha presentado #######1 .',]);
-    InLineaBase::create(['s_linea_base' => 'El AJ #######1 debido a su #######2.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de abandono del(os) representantes legales y/o cuidadores.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de baja supervisión de los NNA por parte de sus cuidadores y/o representantes legales que generan factores de riesgo.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de falta de corresponsabilidad de los representantes legales para asumir el cuidado de sus hijos, asignandolo a otras personas.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de relaciones familiares conflictivas y/o distantes #######1 .',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de #######1 con escasa #######2.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación problemas en la solución de conflictos debido a los inadecuados canales de comunicación.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Existe desconocimiento de normas y limites al interior del nucleo familiar, ########1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de violencia intrafamiliar ejercido por #######1.',]);
-    InLineaBase::create(['s_linea_base' => 'Identificación de violencia intrafamiliar #######1.',]);
+    InLineaBase::create(['s_linea_base' => 'El NNAJ presenta dificultad para interactuar con las demás personas en el #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El NNAJ presenta #######1 para relacionarse con los demás.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El NNAJ reacciona de manera #######1 ante eventos o situaciones que generan cambio emocional.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El NNAJ no cuentan con redes sociales de apoyo.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'La familia del NNAJ no cuenta con redes sociales de apoyo.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => '#######1 presenta dificultades de acceso a los servicios brindados por las redes sociales de apoyo.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'La red de apoyo social representa factores de riesgo para el NNAJ y/o su familia.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El NNA ha sido victima de la ESCNNA #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de ESCNNA en la modalidad de ########', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El NNA ha tenido #######1 de quitarse la vida #######2 por eventos relacionados con su situación como victima de la ESCNNA.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de factores de riesgo de ESCNNA #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de factores de riesgo de ESCNNA naturalizados por parte de la familia como parte de su situación cotidiana.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de presunto abuso sexual #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de evento traumático ante presunto abuso sexual, sin apoyo #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de evento traumático ante presunto abuso sexual #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de #######1, Ante el evento el NNAJ ha emitido #######2 de quitarse la vida.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El AJ ha presentado #######1 .', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'El AJ #######1 debido a su #######2.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de abandono del(os) representantes legales y/o cuidadores.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de baja supervisión de los NNA por parte de sus cuidadores y/o representantes legales que generan factores de riesgo.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de falta de corresponsabilidad de los representantes legales para asumir el cuidado de sus hijos, asignandolo a otras personas.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de relaciones familiares conflictivas y/o distantes #######1 .', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de #######1 con escasa #######2.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación problemas en la solución de conflictos debido a los inadecuados canales de comunicación.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Existe desconocimiento de normas y limites al interior del nucleo familiar, ########1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de violencia intrafamiliar ejercido por #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
+    InLineaBase::create(['s_linea_base' => 'Identificación de violencia intrafamiliar #######1.', 'user_crea_id' => 1, 'user_edita_id' => 1, 'sis_esta_id' => 1]);
   }
 }
