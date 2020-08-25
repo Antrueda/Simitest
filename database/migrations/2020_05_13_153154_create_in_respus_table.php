980e099d9b1282ccb0b3de1c5c3fe46daca6eb44
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-08-25 17:23:32 -0500

    Verificación y correccion de los archivos existentes en Seeds, Migration, Models y Observer; asociados a la tabla in_respus

--- database/migrations/2020_05_13_153154_create_in_respus_table.php
+++ database/migrations/2020_05_13_153154_create_in_respus_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CamposMagicos\CamposMagicos;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -19,14 +20,10 @@ class CreateInRespusTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('in_doc_pregunta_id')->unsigned();
             $table->bigInteger('prm_respuesta_id')->unsigned();
-            $table->Integer('user_crea_id');
-            $table->integer('user_edita_id');
-            $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
-            $table->timestamps();
             $table->foreign('prm_respuesta_id')->references('id')->on('parametros');
             $table->foreign('in_doc_pregunta_id')->references('id')->on('in_doc_preguntas');
             $table->unique(['prm_respuesta_id', 'in_doc_pregunta_id']);
+            $table = CamposMagicos::magicos($table);
         });
         DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LAS RESPUESTAS BRINDADAS A LAS PREGUNTAS ESTABLECIDAS EN EL SISTEMA'");
     }
