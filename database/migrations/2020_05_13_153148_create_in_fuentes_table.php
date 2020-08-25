23626f0003de567009fe14eb4bc2c319673d6932
Nelson Javier Bahamón Parrado <xjavierb@gmail.com>
2020-08-25 12:50:03 -0500

    Verificación y correccion de los archivos existentes en Seeds, Migration, Models y Observer; asociados a la tabla in_fuentes

--- database/migrations/2020_05_13_153148_create_in_fuentes_table.php
+++ database/migrations/2020_05_13_153148_create_in_fuentes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CamposMagicos\CamposMagicos;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -19,16 +20,10 @@ class CreateInFuentesTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('in_linea_base_id')->unsigned();
             $table->bigInteger('in_indicador_id')->unsigned();
-            $table->bigInteger('user_crea_id')->unsigned();
-            $table->bigInteger('user_edita_id')->unsigned();
-            $table->bigInteger('sis_esta_id')->unsigned()->default(1);
-            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
-            $table->timestamps();
             $table->foreign('in_linea_base_id')->references('id')->on('in_linea_bases');
             $table->foreign('in_indicador_id')->references('id')->on('in_indicadors');
-            $table->foreign('user_crea_id')->references('id')->on('users');
-            $table->foreign('user_edita_id')->references('id')->on('users');
             $table->unique(['in_indicador_id', 'in_linea_base_id']);
+            $table = CamposMagicos::magicos($table);
         });
         DB::statement("ALTER TABLE `{$this->tablaxxx}` comment 'TABLA QUE ALMACENA LA RELACION ENTRE LA LINEA DE BASE DE LOS BENEFICIARIOS DE LOS SERVICIOS DEL IDIPRON CON LOS INDICADOR OBSERVADO POR EL FUNCIONARIO'");
     }
