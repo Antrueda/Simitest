efab17e01481a886e462a5f49aadc98dfc02f512
Ing. José Dúmar Jiménez Ruíz <josej@idipron.gov.co>
2020-09-03 17:08:58 -0500

    cambios

--- app/Models/fichaIngreso/FiDatosBasico.php
+++ app/Models/fichaIngreso/FiDatosBasico.php
@@ -185,6 +185,8 @@ class FiDatosBasico extends Model
                     $objetoxx->nnaj_fi_csd->update($dataxxxx);
 
 
+
+
             } else {
                 /** Es un registro nuevo */
                 $dataxxxx['sis_nnaj_id'] = SisNnaj::create(['sis_esta_id'=>1,'user_crea_id' => Auth::user()->id, 'user_edita_id' => Auth::user()->id])->id;
