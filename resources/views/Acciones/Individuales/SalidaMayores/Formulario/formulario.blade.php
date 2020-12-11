6fe00000985b995b73b421ba8024bb66e843b890
Jorge Rueda <antrueda14@gmail.com>
2020-12-11 18:02:33 -0500

    Se corrigio salidas a mayores

--- resources/views/Acciones/Individuales/SalidaMayores/Formulario/formulario.blade.php
+++ resources/views/Acciones/Individuales/SalidaMayores/Formulario/formulario.blade.php
@@ -1,7 +1,7 @@
 
 <div class="row">
     <div class="col-md-4">
-        {{ Form::label('fecha', 'Fecha', ['class' => 'control-label col-form-label-sm']) }}
+        {{ Form::label('fecha', 'Fecha de Diligenciamiento', ['class' => 'control-label col-form-label-sm']) }}
         {{ Form::date('fecha', null, ['class' => $errors->first('fecha') ? 'form-control form-control-sm is-invalid' : 'form-control form-control-sm','max' => $todoxxxx['hoyxxxxx']]) }}
         @if($errors->has('fecha'))
             <div class="invalid-feedback d-block">
@@ -10,7 +10,7 @@
         @endif
     </div>
     <div class="col-md-4">
-        {{ Form::label('prm_upi_id', 'UPI / Dependencia', ['class' => 'control-label col-form-label-sm']) }}
+        {{ Form::label('prm_upi_id', 'UPI', ['class' => 'control-label col-form-label-sm']) }}
         {{ Form::select('prm_upi_id', $todoxxxx['dependen'], null, ['class' => $errors->first('prm_upi_id') ? 'form-control select2 form-control-sm is-invalid' : 'form-control select2 form-control-sm', 'data-placeholder' => 'Seleccione la UPI']) }}
         @if($errors->has('prm_upi_id'))
             <div class="invalid-feedback d-block">
@@ -54,7 +54,8 @@
     <div class="col-md-10">
 		{{ Form::label('descripcion', 'Descripción / Razón', ['class' => 'control-label col-form-label-sm']) }}
 		{{ Form::textarea('descripcion', null, ['class' => $errors->first('descripcion') ? 'form-control form-control-sm is-invalid' : 'form-control form-control-sm', 'placeholder' => 'Descripción', 'maxlength' => '4000', 'autofocus', 'onkeyup' => 'javascript:this.value=this.value.toUpperCase();', 'style' => 'text-transform:uppercase;']) }}
-		@if($errors->has('descripcion'))
+        <p id="contadordescripcion">0/4000</p>
+        @if($errors->has('descripcion'))
 			<div class="invalid-feedback d-block">
 	        	{{ $errors->first('descripcion') }}
 	      	</div>
@@ -63,9 +64,9 @@
 </div>
 <br>
 
-<h6 class="mt-3">Firma</h6>
+<h6 class="mt-3">FIRMA</h6>
 
-{{ Form::label(null, 'Funcionario(a)/Contratista', ['class' => 'control-label col-form-label-sm']) }}
+{{ Form::label(null, 'FUNCIONARIO(A)/CONTRATISTA', ['class' => 'control-label col-form-label-sm']) }}
 <div class="row">
 	<div class="col-md-10">
 		{{ Form::label('user_doc1_id', 'Número de Documento - Nombres y apellidos - Cargo', ['class' => 'control-label col-form-label-sm']) }}
