f9cea65881c04ee5a0721db41a7753bc09940146
Jorge Rueda <antrueda14@gmail.com>
2021-02-24 18:41:53 -0500

    se realizo correciones busqueda cargue de documentos

--- app/Traits/Administracion/Carguedocu/ListadosTrait.php
+++ app/Traits/Administracion/Carguedocu/ListadosTrait.php
@@ -4,10 +4,11 @@ namespace App\Traits\Administracion\Carguedocu;
 
 use App\Models\fichaIngreso\FiDatosBasico;
 use App\Models\fichaIngreso\FiDocumentosAnexa;
+use App\Models\Sistema\SisDepeUsua;
 use App\Models\Sistema\SisNnaj;
 use App\Traits\DatatableTrait;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Este trait permite armar las consultas para ubicacion que arman las datatable
@@ -20,6 +21,17 @@ trait ListadosTrait
      * encontrar listar paises
      */
 
+     
+    public function getNotInt()
+    {
+        $userxxxx = Auth::user();
+        $userxxxx = SisDepeUsua::select('sis_depen_id')->where(function ($queryxxx) use ($userxxxx) {
+            $queryxxx->where('user_id', $userxxxx->id);
+            $queryxxx->where('sis_esta_id', 1);
+        })->get();
+
+        return $userxxxx;
+    }
     public function listaNnaj(Request $request)
     {
 
@@ -46,8 +58,19 @@ trait ListadosTrait
                 ->join('sis_nnajs', 'fi_datos_basicos.sis_nnaj_id', '=', 'sis_nnajs.id')
                 ->join('nnaj_docus', 'fi_datos_basicos.id', '=', 'nnaj_docus.fi_datos_basico_id')
                 ->join('sis_estas', 'fi_datos_basicos.sis_esta_id', '=', 'sis_estas.id')
-                // ->join('nnaj_upis', 'fi_datos_basicos.sis_nnaj_id', '=', 'nnaj_upis.sis_nnaj_id')
-                ->where('sis_nnajs.prm_escomfam_id', 227);
+                ->join('nnaj_upis', 'fi_datos_basicos.sis_nnaj_id', '=', 'nnaj_upis.sis_nnaj_id')
+                ->where('sis_nnajs.prm_escomfam_id', 227)
+                ->whereIn('nnaj_upis.sis_depen_id', $this->getNotInt())->groupBy([
+                    'fi_datos_basicos.sis_nnaj_id', 'fi_datos_basicos.id', 'fi_datos_basicos.s_primer_nombre',
+                    'nnaj_docus.s_documento',
+                    'fi_datos_basicos.s_segundo_nombre',
+                    'fi_datos_basicos.s_primer_apellido',
+                    'fi_datos_basicos.s_segundo_apellido',
+                    'fi_datos_basicos.sis_esta_id',
+                    'fi_datos_basicos.created_at',
+                    'sis_estas.s_estado',
+                    'fi_datos_basicos.user_crea_id',
+                ]);
 
             return $this->getDtUpi($dataxxxx, $request);
         }
