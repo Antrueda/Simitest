1ca7106152db6155014645ea8b790f9bebd78666
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2021-05-12 21:27:54 -0500

    se realiza corrección a error presentado al pasar el consumo de spa del sistema nuevo al antiguo

--- app/Traits/Fi/Datobasi/DBControllerTrait.php
+++ app/Traits/Fi/Datobasi/DBControllerTrait.php
@@ -6,8 +6,11 @@ use App\Http\Requests\FichaIngreso\FiDatosBasicoCrearRequest;
 use App\Http\Requests\FichaIngreso\FiDatosBasicoMigrarCrearRequest;
 use App\Http\Requests\FichaIngreso\FiDatosBasicoUpdateRequest;
 use App\Models\fichaIngreso\FiDatosBasico;
+use App\Models\Parametro;
 use App\Models\Simianti\Ge\GeNnajDocumento;
 use App\Models\Simianti\Sis\SisMultivalore;
+use App\Models\Simianti\Sis\SisSpa;
+use App\Models\Sistema\SisNnaj;
 use App\Models\Temacombo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -89,7 +92,7 @@ trait DBControllerTrait
         if (isset($document->id_nnaj)) {
             $this->getUpisModalidadHT(['idnnajxx' => $document->id_nnaj, 'sisnnaji' => $objetoxx->sis_nnaj_id]);
         }
-        if ($objetoxx->sis_nnaj->prm_nuevoreg_id == 228) {
+        if ($objetoxx->sis_nnaj->simianti_id <1) {
             $this->setNnajAnguoSimiIFT(['padrexxx' => $objetoxx]);
         }
 
@@ -149,7 +152,7 @@ trait DBControllerTrait
     public function getArmaCamposTabalSimiAnti()
     {
 
-// $tamacomb = Temacombo::find(53);
+        // $tamacomb = Temacombo::find(53);
 
         // foreach (SisSpa::get(['id_spa', 'nombre_spa']) as $key => $value) {
         //     $parametu = Parametro::where('nombre', $value->nombre_spa)->first();
@@ -159,23 +162,28 @@ trait DBControllerTrait
         //         $parametu->sis_esta_id = 1;
         //         $parametu->user_crea_id = Auth::user()->id;
         //         $parametu->user_edita_id = Auth::user()->id;
+        //         echo "$value->id_spa {$value->nombre_spa}<br>";
         //     } else {
         //         $paraexis = $tamacomb->parametros->where('nombre', $value->nombre_spa)->first();
         //         if ($paraexis != null) {
-        //             echo "$value->id_spa {$value->nombre_spa}<br>";
-        //             $tamacomb->parametros()->updateExistingPivot($paraexis->id, ['simianti_id' => $value->id_spa, 'user_edita_id' => Auth::user()->id], false);
+        //             // echo "$value->id_spa {$value->nombre_spa}<br>";
+        //             // $tamacomb->parametros()->updateExistingPivot($paraexis->id, ['simianti_id' => $value->id_spa, 'user_edita_id' => Auth::user()->id], false);
+        //         } else {
+        //             // echo "$value->id_spa {$value->nombre_spa}<br>";
         //         }
         //     }
         // }
         // echo '<br>';
         // foreach ($tamacomb->parametros as $key => $value) {
         //     if ($value->pivot->simianti_id < 1) {
-        //         echo "{$value->nombre} <br>";
+        //         echo "$value->id {$value->nombre} <br>";
+        //     } else {
+        //         // echo "$value->id {$value->nombre} <br>";
         //     }
         // }
 
-        // // $tamacomb->parametros()
-        // // ->updateExistingPivot(406, ['simianti_id' => 3, 'user_edita_id' => Auth::user()->id], false);
+        // $tamacomb->parametros()
+        //     ->updateExistingPivot(2465, ['simianti_id' => 35, 'user_edita_id' => Auth::user()->id], false);
         // ddd(4);
 
 
@@ -185,7 +193,7 @@ trait DBControllerTrait
         //        echo "'$key',<br>";
         //     }
         // echo '];';
-        //     ddd($dd->toArray());
+
     }
 
     public function prueba($temaxxxx, $tablaxxx, Request $request)
@@ -194,7 +202,7 @@ trait DBControllerTrait
         // php artisan vendor:publish --provider="BeyondCode\QueryDetector\QueryDetectorServiceProvider"
 
         $this->getArmaCamposTabalSimiAnti();
-        $this->getRocorrerCedula();
+        // $this->getRocorrerCedula();
         // $i = 1;
         // $tables = DB::select('SHOW TABLES');
         // foreach ($tables as $key => $data) {
@@ -290,7 +298,7 @@ trait DBControllerTrait
         }
 
 
-        return $this->view(['modeloxx' => '', 'accionxx' => ['homologa', 'homologa']]);
+        // return $this->view(['modeloxx' => '', 'accionxx' => ['homologa', 'homologa']]);
     }
     public function homologa($temacomb, $parametr, $codigoxx, $tablaxxx)
     {
--- app/Traits/Interfaz/Antisimi/ConsumoSpaTrait.php
+++ app/Traits/Interfaz/Antisimi/ConsumoSpaTrait.php
@@ -2,10 +2,11 @@
 
 namespace App\Traits\Interfaz\Antisimi;
 
+use App\Models\Simianti\Ge\GeNnajDocumento;
 use App\Models\Simianti\Tr\TrSeguiConsumoSpa;
 use App\Models\Simianti\Tr\TrVespa;
 use App\Models\Temacombo;
-
+use Illuminate\Support\Facades\Auth;
 
 /**
  * realiza la migracion del consummo spa al antiguo simi
@@ -20,7 +21,11 @@ trait ConsumoSpaTrait
      */
     public function setTrVespaCST($dataxxxx)
     {
+
         $nnajxxxx = $dataxxxx['consumox'];
+        $sisnnajx = $nnajxxxx->sis_nnaj;
+        $document = GeNnajDocumento::where('numero_documento', $sisnnajx->fi_datos_basico->nnaj_docu->s_documento)->first();
+        $sisnnajx->update(['simianti_id' => $document->id_nnaj, 'user_edita_id' => Auth::user()->id]);
         $vespaxxx = TrVespa::where('id_nnaj', $nnajxxxx->sis_nnaj->simianti_id)
             ->first();
         if ($vespaxxx == null) {
@@ -34,7 +39,7 @@ trait ConsumoSpaTrait
             $vespaxxx->fecha_consulta = explode(' ', $nnajxxxx->created_at)[0];
             $vespaxxx->id_ocupac_oficio = 0;
             $vespaxxx->id_nnaj = $nnajxxxx->sis_nnaj->simianti_id;
-            // $vespaxxx->save();
+            $vespaxxx->save();
         }
         return  $vespaxxx;
     }
@@ -56,8 +61,7 @@ trait ConsumoSpaTrait
         $vespaxxx = $this->setTrVespaCST($dataxxxx);
         $nnajxxxx = $dataxxxx['consumox']->fi_sustancia_consumidas;
         $vespaxxy = $vespaxxx->tr_segui_consumo_spa->toArray();
-        if (
-            count($nnajxxxx->toArray()) > 0 && count($vespaxxy)==0) {
+        if (count($nnajxxxx->toArray()) > 0 && count($vespaxxy) == 0) {
             foreach ($nnajxxxx as $key => $value) {
                 $trsecons = new TrSeguiConsumoSpa();
                 $trsecons->id_vespa = $vespaxxx->id_vespa;
@@ -67,13 +71,13 @@ trait ConsumoSpaTrait
                 $trsecons->fecha_modificacion = $value->updated_at;
                 $trsecons->usuario_modificacion = $value->user_edita->s_documento;
                 $trsecons->id_nnaj = $vespaxxx->id_nnaj;
+                $trsecons->fecha_seguimiento =$value->created_at;
                 $trsecons->id_segui_consumo_spa = TrSeguiConsumoSpa::select(['id_segui_consumo_spa'])->orderBy('id_segui_consumo_spa', 'DESC')->first()->id_segui_consumo_spa + 1;
-                // ddd($trsecons);
                 $trsecons->save();
             }
         }
     }
-
+    
     public function setConsumoCST($dataxxxx)
     {
         $this->setTrSeguiConsumoSpaCST($dataxxxx);
