e9ea9400d6a85e1b98f092cb55735edebc660beb
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-05-26 21:47:54 -0500

    Separacion de las respuestas multiples a pestañas

---
+++ app/Exports/CaminandoRelajado/ActividadesTiempoLibre/ReporteActividadesTiempoLibreSheet.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Exports\CaminandoRelajado\ActividadesTiempoLibre;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class ReporteActividadesTiempoLibreSheet implements FromView, ShouldAutoSize, WithStyles, WithTitle
+{
+    private $sisNnajs;
+
+    public function __construct($sisNnajs)
+    {
+        $this->sisNnajs = $sisNnajs;
+    }
+
+    public function view(): View
+    {
+        return view('administracion.Reportes.Proyectos.export.CaminandoRelajado.ActividadesTiempoLibre.actividadesTiempoLibreView', [
+            'sisNnajs'      => $this->sisNnajs,
+        ]);
+    }
+
+    public function title(): string
+    {
+        return '8. Actividades de tiempo libre.';
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('1')->getFont()->setBold(true);
+        $sheet->getStyle('1')->getFont()->getColor()->setARGB('c2c7d0');
+        $sheet->getStyle('1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
+        $sheet->getStyle('1')->getFill()->getStartColor()->setARGB('343a40');
+    }
+
+}
