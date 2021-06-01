790653cfc4160cab3fe2095bb5b8f0c884334a9d
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-06-01 13:46:41 -0500

    Ajustes de las vistas para reportes

--- app/Models/sistema/SisNnaj.php
+++ app/Models/sistema/SisNnaj.php
@@ -52,7 +52,7 @@ class SisNnaj extends Model
 
     public function FiResidencia()
     {
-        return $this->hasMany(FiResidencia::class, 'sis_nnaj_id');
+        return $this->hasOne(FiResidencia::class, 'sis_nnaj_id');
     }
 
     public function getNnajDatosAttribute()
