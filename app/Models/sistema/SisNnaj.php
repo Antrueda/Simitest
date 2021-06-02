7ad661e8e42e92aae4f282c8ebc779d089a134ce
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-06-02 13:06:49 -0500

    Implementando filtro de estrategia

--- app/Models/sistema/SisNnaj.php
+++ app/Models/sistema/SisNnaj.php
@@ -226,7 +226,7 @@ class SisNnaj extends Model
 
     public function nnaj_depes()
     {
-        return $this->hasMany(NnajUpi::class);
+        return $this->hasOne(NnajUpi::class);
     }
     public function fi_compfamis()
     {
