185bbe5ee76f55a59948c1c361a82b7f8d86cac8
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-06-03 09:31:17 -0500

    Ajustes

--- resources/views/administracion/Reportes/Proyectos/export/CaminandoRelajado/datosDeIdentificacionBody.blade.php
+++ resources/views/administracion/Reportes/Proyectos/export/CaminandoRelajado/datosDeIdentificacionBody.blade.php
@@ -1,16 +1,16 @@
 {{-- id_nnja --}}
-<td>{{ $sisNnaj->id }}</td>
+<td>{{ $sisNnaj->id ?? 'Sin dato' }}</td>
 {{-- primer nombre --}}
-<td>{{ $sisNnaj->fi_datos_basico->s_primer_nombre }}</td>
+<td>{{ $sisNnaj->fi_datos_basico->s_primer_nombre ?? 'Sin dato' }}</td>
 {{-- segundo nombre --}}
-<td>{{ $sisNnaj->fi_datos_basico->s_segundo_nombre }}</td>
+<td>{{ $sisNnaj->fi_datos_basico->s_segundo_nombre ?? 'Sin dato' }}</td>
 {{-- primer apellido --}}
-<td>{{ $sisNnaj->fi_datos_basico->s_primer_apellido }}</td>
+<td>{{ $sisNnaj->fi_datos_basico->s_primer_apellido ?? 'Sin dato' }}</td>
 {{-- segundo apellido --}}
-<td>{{ $sisNnaj->fi_datos_basico->s_segundo_apellido }}</td>
+<td>{{ $sisNnaj->fi_datos_basico->s_segundo_apellido ?? 'Sin dato' }}</td>
 {{-- 1.11 Documento con el cual se identifica --}}
-<td>{{ $sisNnaj->fi_datos_basico->nnaj_docu->tipoDocumento->nombre }}</td>
+<td>{{ $sisNnaj->fi_datos_basico->nnaj_docu->tipoDocumento->nombre ?? 'Sin dato' }}</td>
 {{-- numero de documento --}}
-<td>{{ $sisNnaj->fi_datos_basico->nnaj_docu->s_documento }}</td>
+<td>{{ $sisNnaj->fi_datos_basico->nnaj_docu->s_documento ?? 'Sin dato' }}</td>
 {{-- upi actual --}}
 <td>{{ $sisNnaj->nnaj_depes->sis_depen->nombre ?? 'Sin dependencia' }}</td>
