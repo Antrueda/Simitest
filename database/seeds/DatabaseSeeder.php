659b437f568ec1f538ab0dd1c1b0523b192ad265
antrueda14 <antrueda14@gmail.com>
2021-06-30 18:29:37 -0500

    se agregaron seeders produccion temacombo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -49,10 +49,15 @@ class DatabaseSeeder extends Seeder
         $this->call(SisEslugSeeder::class);
         $this->call(TemasTableSeeder::class);
         $this->call(TemacomboSeeder::class);
+        //Produccion Seeders tema
+        $this->call(TemacomboProduccionSeeder::class);
+
 
         $this->call(ParametroTemacomboMilSeeder::class);
         $this->call(ParametroTemacomboDosmilSeeder::class);
         $this->call(ParametroTemacomboTresmilSeeder::class);
+        //Produccion Seeders parametemacombo
+        $this->call(ParametroTemacomboProducSeeder::class);
 
         $this->call(SisAreasSeeder::class);
         $this->call(AreasUserSeeder::class);
@@ -252,6 +257,9 @@ class DatabaseSeeder extends Seeder
         $this->call(MotivoEgresoSeeder::class);  // Seeder Motivo de egreso
         $this->call(MotivoEgresoSecusSeeder::class);  // Seeder Motivo de egreso secundario
         $this->call(MotivoEgreusSeeder::class);  // Seeder Motivo egreso union
+
+
+       
 
     }
 }
