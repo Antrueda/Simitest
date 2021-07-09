15dec63fe595179ec6251d1c54c43d1d180ab743
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-07-09 12:05:14 -0500

    se realiza corrección de errores en la migraciones y el permiso al mostrarla en el menú

--- resources/views/layouts/menus/territorio.blade.php
+++ resources/views/layouts/menus/territorio.blade.php
@@ -25,7 +25,7 @@
              </a>
          </li>
          @endcan
-         @can('cargdocu-moduloxx')
+         @can('actamodu-moduloxx')
          <li class="nav-item">
             <a href="{{ route('actamodu') }}" class="nav-link">
                 <i class="fas fa-address-card nav-icon"></i>
