4d13600ca9e8218453f3ddf71602f4c17c9b9054
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-07-12 13:56:48 -0500

    Acta de encuentro

--- resources/views/Actaencu/Actaencu/Js/js.blade.php
+++ resources/views/Actaencu/Actaencu/Js/js.blade.php
@@ -129,19 +129,19 @@
             });
         });
 
-    });
-
-    $(function(){
         $('.select2').select2({
             language: "es"
         });
-        $("#s_doc_adjunto_ar").change(function() {
-            var fichero_seleccionado = $(this).val();
-            var nombre_fichero_seleccionado = fichero_seleccionado.replace(/.*[\/\\]/, ''); //Eliminamos el path hasta el fichero seleccionado
-            $("#docontacto").text(nombre_fichero_seleccionado);
-        });
-
-
-
     });
+
+    // $(function(){
+    //     $("#s_doc_adjunto_ar").change(function() {
+    //         var fichero_seleccionado = $(this).val();
+    //         var nombre_fichero_seleccionado = fichero_seleccionado.replace(/.*[\/\\]/, ''); //Eliminamos el path hasta el fichero seleccionado
+    //         $("#docontacto").text(nombre_fichero_seleccionado);
+    //     });
+
+
+
+    // });
 </script>
