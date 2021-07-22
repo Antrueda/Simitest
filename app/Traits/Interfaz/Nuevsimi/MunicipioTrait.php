f977aa49ea5ccb15762318b8967a0a4ccdc1feec
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-07-21 20:38:56 -0500

    se implementa trait que realiza las validaciones para homologar un municipio del antiguo desarrollo al nuevo desarrollo

---
+++ app/Traits/Interfaz/Nuevsimi/MunicipioTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Traits\Interfaz\Nuevsimi;
+
+use App\Models\Simianti\Sis\Municipio;
+use app\Models\sistema\SisMunicipio;
+
+
+/**
+ * realiza la validaciones para homologar un municipio del antiguo desarrollo al nuevo desarrollo
+ *
+ */
+trait MunicipioTrait
+{
+    /**
+     * homologar municipio
+     *
+     * @param array $dataxxxx
+     * @return object $muninuev
+     */
+    public function getMunicipoSimi($dataxxxx)
+    {
+        $munianti = Municipio::find($dataxxxx['idmunici']);
+        if ($dataxxxx['idmunici'] == 11001) {
+            $muninuev = SisMunicipio::find(231);
+        } elseif (!isset($munianti->codigo_municipio)) {
+            $muninuev = SisMunicipio::find(1121);
+        } else {
+            $muninuev = SisMunicipio::where('s_municipio', $munianti->nombre_municipio)->first();
+        }
+        if (!isset($muninuev->id)) {
+            $muninuev = SisMunicipio::find(1121);
+        }
+        return $muninuev;
+    }
+}
