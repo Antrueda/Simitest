cd4e40367329db4def7b46d01a5fcb7b80890934
Veronica Borges <ingveronicab@gmail.com>
2021-08-09 20:41:37 -0500

    Se realizan mejoras de Evasión donde se ajustaron temas de acento, se quito datos basicos que sobraba, se organizaron los parametros de tema combo.

--- app/Http/Controllers/Acciones/Individuales/AIEvasionController.php
+++ app/Http/Controllers/Acciones/Individuales/AIEvasionController.php
@@ -86,7 +86,7 @@ class AIEvasionController extends Controller
                     [
                         ['td' => 'ACCIONES', 'widthxxx' => 200, 'rowspanx' => 1, 'colspanx' => 1],
                         ['td' => 'ID', 'widthxxx' => 0, 'rowspanx' => 1, 'colspanx' => 1],
-                        ['td' => 'FECHA DE EVASION', 'widthxxx' => 0, 'rowspanx' => 1, 'colspanx' => 1],
+                        ['td' => 'FECHA DE EVASIÓN', 'widthxxx' => 0, 'rowspanx' => 1, 'colspanx' => 1],
                         ['td' => 'HORA DE EVASIÓN', 'widthxxx' => 0, 'rowspanx' => 1, 'colspanx' => 1],
                         ['td' => 'LUGAR DE EVASIÓN', 'widthxxx' => 0, 'rowspanx' => 1, 'colspanx' => 1],
                         ['td' => 'UPI', 'widthxxx' => 0, 'rowspanx' => 1, 'colspanx' => 1],
