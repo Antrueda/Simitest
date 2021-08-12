c208b4b4fd0049ddcf5c5618392db6d935496035
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-08-12 10:32:08 -0500

    cambio

--- routes/Actaencu/web_actamodu.php
+++ routes/Actaencu/web_actamodu.php
@@ -13,16 +13,9 @@ Route::group(['prefix' => 'moduloae'], function () use ($routexxx, $controll) {
         'middleware' => ['permission:' . $routexxx . '-leerxxxx|' . $routexxx . '-crearxxx|' . $routexxx . '-editarxx|' . $routexxx . '-borrarxx']
     ])->name($routexxx . '.listaxxx');
     require_once('web_actaencu.php');
-<<<<<<< HEAD
-    require_once('web_aerecurs.php');
-    require_once('web_asistenc.php');
-    require_once('web_aecontac.php');
-    require_once('web_asisnnaj.php');
-=======
     require_once('web_aecontac.php');
     require_once('web_asistenc.php');
     require_once('web_asisnnaj.php');
     require_once('web_aerecadm.php');
     require_once('web_aerecurs.php');
->>>>>>> d93c0f82f90075c9d9b4f4053fba6f75dd587397
 });
