dcf07412d086ecb97a2d2f6d4c337c63726c1bb1
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-08-12 16:41:44 -0500

    se realiza desarrollo para que se le puedan asiginar recursos al acta de encuentro

--- app/Traits/Combos/CombosTrait.php
+++ app/Traits/Combos/CombosTrait.php
@@ -329,6 +329,7 @@ trait CombosTrait
             ->join('sis_depeservs', 'sis_depens.id', '=', 'sis_depeservs.sis_depen_id')
             ->whereIn('prm_tvinculacion_id', [1673, 1674])
             ->where('sis_depeservs.sis_servicio_id', 6)
+            ->where('sis_depen_user.sis_esta_id', 1)
             ->get(['users.name as optionxx', 'users.id as valuexxx', 's_documento']);
         $respuest = ['comboxxx' => $this->getCuerpoUsuarioCT($dataxxxx)];
         return $respuest;
