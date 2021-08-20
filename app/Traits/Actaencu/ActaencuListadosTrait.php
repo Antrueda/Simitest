a186bcc399cfcbb75f8cda53d6fe8d1964db2ba4
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-08-20 11:07:56 -0500

    Ajustes solicitados

--- app/Traits/Actaencu/ActaencuListadosTrait.php
+++ app/Traits/Actaencu/ActaencuListadosTrait.php
@@ -163,7 +163,7 @@ trait ActaencuListadosTrait
         if ($request->ajax()) {
             $request->routexxx = [$this->opciones['routxxxx'], 'comboxxx'];
             $request->botonesx = $this->opciones['rutacarp'] .
-                $this->opciones['carpetax'] . '.Botones.botonesnnajapi';
+                $this->opciones['carpetax'] . '.Botones.botonesnnajasigapi';
             $request->estadoxx = 'layouts.components.botones.estadosx';
 
             $dataxxxx =  FiDatosBasico::select([
@@ -218,7 +218,7 @@ trait ActaencuListadosTrait
         if ($request->ajax()) {
             $request->routexxx = [$this->opciones['routxxxx'], 'comboxxx'];
             $request->botonesx = $this->opciones['rutacarp'] .
-                $this->opciones['carpetax'] . '.Botones.botonesnnajapi';
+                $this->opciones['carpetax'] . '.Botones.botonesnnajelimapi';
             $request->estadoxx = 'layouts.components.botones.estadosx';
 
             $dataxxxx =  FiDatosBasico::select([
