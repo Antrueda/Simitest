73c6003994a471172a646d4958b95313e6b58a86
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-08-26 17:47:09 -0500

    Ajustes

--- app/Http/Controllers/Actaencu/AeAsisNnajsController.php
+++ app/Http/Controllers/Actaencu/AeAsisNnajsController.php
@@ -80,7 +80,7 @@ class AeAsisNnajsController extends Controller
         $this->pestania[2][2]=[$padrexxx->id];
         $this->opciones['parametr'][]=$padrexxx->id;
         $this->getBotones(['editarxx', [], 1, 'EDITAR CONTACTO', 'btn btn-sm btn-primary']);
-        $this->getBotones(['editarxx', ['asistenc.editarxx', [$padrexxx->aeEncuentro->id, $padrexxx->id]], 2, 'VOLVER A ASISTNCIA', 'btn btn-sm btn-primary']);
+        $this->getBotones(['editarxx', ['asistenc.editarxx', [$padrexxx->id]], 2, 'VOLVER A ASISTNCIA', 'btn btn-sm btn-primary']);
         return $this->view(['modeloxx' => $modeloxx, 'accionxx' => ['editarxx', 'formulario'], 'todoxxxx' => $this->opciones, 'padrexxx' => $padrexxx]);
     }
 
