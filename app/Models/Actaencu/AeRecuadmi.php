c7ad678a47a6e9aef12bd38e113c85f747786c97
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-08-31 12:01:46 -0500

    se crea modelo AeRecuadmi

--- app/Models/Actaencu/AeRecuadmi.php
+++ app/Models/Actaencu/AeRecuadmi.php
@@ -2,9 +2,53 @@
 
 namespace App\Models\Actaencu;
 
+use App\Models\Parametro;
+use App\Models\sistema\SisEsta;
+use App\Models\Usuario\Estusuario;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AeRecuadmi extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        's_recurso',
+        'prm_trecurso_id',
+        'prm_umedida_id',
+        'estusuario_id',
+        'sis_esta_id',
+        'user_crea_id',
+        'user_edita_id'
+    ];
+
+    public function prm_trecurso()
+    {
+        return $this->belongsTo(Parametro::class, 'prm_trecurso_id');
+    }
+
+    public function prm_umedida()
+    {
+        return $this->belongsTo(Parametro::class, 'prm_umedida_id');
+    }
+
+    public function estusuario()
+    {
+        return $this->belongsTo(Estusuario::class);
+    }
+
+    public function userCrea()
+    {
+        return $this->belongsTo(User::class, 'user_crea_id');
+    }
+
+    public function userEdita()
+    {
+        return $this->belongsTo(User::class, 'user_edita_id');
+    }
+
+    public function sis_esta()
+    {
+        return $this->belongsTo(SisEsta::class);
+    }
 }
