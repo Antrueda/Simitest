fe7a4db8a2bacea43602a3a25d3650f82b81c636
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-09-01 16:20:30 -0500

    Se crean migraciones de tablas para el historial de acta de encuentro

--- database/migrations/2021_07_19_141400_create_ae_asistecias_table.php
+++ database/migrations/2021_07_19_141400_create_ae_asistecias_table.php
@@ -30,6 +30,24 @@ class CreateAeAsisteciasTable extends Migration
             $table->foreign('user_crea_id')->references('id')->on('users');
             $table->foreign('user_edita_id')->references('id')->on('users');
         });
+
+        Schema::create('h_ae_asistencias', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ae_encuentro_id')->unsigned();
+            $table->integer('user_funcontr_id')->unsigned();
+            $table->integer('respoupi_id')->unsigned();
+            $table->integer('sis_esta_id')->unsigned()->comment('PARAMETRO TIPO DE AUTORIZACION');
+            $table->integer('user_crea_id')->unsigned()->comment('PARAMETRO TIPO DE AUTORIZACION');
+            $table->integer('user_edita_id')->unsigned()->comment('PARAMETRO TIPO DE AUTORIZACION');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_funcontr_id')->references('id')->on('users');
+            $table->foreign('respoupi_id')->references('id')->on('users');
+            $table->foreign('sis_esta_id')->references('id')->on('sis_estas');
+            $table->foreign('user_crea_id')->references('id')->on('users');
+            $table->foreign('user_edita_id')->references('id')->on('users');
+        });
     }
 
     /**
@@ -40,5 +58,6 @@ class CreateAeAsisteciasTable extends Migration
     public function down()
     {
         Schema::dropIfExists('ae_asistecias');
+        Schema::dropIfExists('h_ae_asistecias');
     }
 }
