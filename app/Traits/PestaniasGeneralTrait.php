dce5f305d437dab2a4a3ab7e1d49808c14bcdcb7
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-09-23 10:32:36 -0500

    se agrega la funcionalidad de los permisos consultados desde la base de datos para las pestañas en el trait general

--- app/Traits/PestaniasGeneralTrait.php
+++ app/Traits/PestaniasGeneralTrait.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use Spatie\Permission\Models\Permission;
+
 /**
  * trait que arma la estructuar para la funcionalidad de las pestañas de manera general
  */
@@ -11,9 +13,7 @@ trait PestaniasGeneralTrait
     private $moduloxx=true;
     private function getCanany($routexxx, $dataxxxx)
     {
-        $permisox = [
-            'leerxxxx', 'crearxxx', 'editarxx', 'borrarxx', 'activarx'
-        ];
+        $permisox =Permission::where('name','like',$routexxx.'%')->get('name');
         $respuest = [];
         foreach ($permisox as $key => $value) {
             $respuest[] = $routexxx . '-' . $value;
