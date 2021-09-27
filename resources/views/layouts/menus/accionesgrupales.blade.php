fa65d895f3e372ec5c9b6197bed625b74b473c81
antrueda14 <antrueda14@gmail.com>
2021-09-27 15:45:40 -0500

    se corrgio js de traslado

--- resources/views/layouts/menus/accionesgrupales.blade.php
+++ resources/views/layouts/menus/accionesgrupales.blade.php
@@ -33,6 +33,7 @@
             </li>
             @endcan
       
+                 @if(in_array(Auth::user()->s_documento,['17496705','1090412429']))
             @can('imatricula-leer')
             <li class="nav-item">
               <a href="{{ route('imatricula')}}" class="nav-link">
@@ -41,7 +42,7 @@
               </a>
             </li>
             @endcan
-
+           @endif
 
             {{-- @if(in_array(Auth::user()->s_documento,['17496705','1090412429'])) --}}
             
