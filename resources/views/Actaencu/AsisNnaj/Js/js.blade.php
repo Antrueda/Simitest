3fc63b690ff579ae52b5236aeadbb6c04699268c
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-10-25 10:17:13 -0500

    Ajuste

--- resources/views/Actaencu/AsisNnaj/Js/js.blade.php
+++ resources/views/Actaencu/AsisNnaj/Js/js.blade.php
@@ -139,19 +139,19 @@
                 $('#i_prm_cuadrante_vg_id').attr('disabled', true);
             } else {
                 console.log(zonadire);
-                $('#s_complemento').attr('disabled', false);
+                $('#s_complemento').removeAttr('disabled');
                 $('#s_nombre_via').val(0);
-                $('#s_nombre_via').attr('readonly', false);
+                $('#s_nombre_via').removeAttr('readonly');
                 $('#i_via_generadora').val(0);
-                $('#i_via_generadora').attr('readonly', false);
+                $('#i_via_generadora').removeAttr('readonly');
                 $('#i_placa_vg').val(0);
-                $('#i_placa_vg').attr('readonly', false);
-                $('#i_prm_alfabeto_via_id').attr('disabled', false);
-                $('#i_prm_tiene_bis_id').attr('disabled', false);
-                $('#i_prm_bis_alfabeto_id').attr('disabled', false);
-                $('#i_prm_cuadrante_vp_id').attr('disabled', false);
-                $('#i_prm_alfabetico_vg_id').attr('disabled', false);
-                $('#i_prm_cuadrante_vg_id').attr('disabled', false);
+                $('#i_placa_vg').removeAttr('readonly');
+                $('#i_prm_alfabeto_via_id').removeAttr('disabled');
+                $('#i_prm_tiene_bis_id').removeAttr('disabled');
+                $('#i_prm_bis_alfabeto_id').removeAttr('disabled');
+                $('#i_prm_cuadrante_vp_id').removeAttr('disabled');
+                $('#i_prm_alfabetico_vg_id').removeAttr('disabled');
+                $('#i_prm_cuadrante_vg_id').removeAttr('disabled');
             }
         });
 
