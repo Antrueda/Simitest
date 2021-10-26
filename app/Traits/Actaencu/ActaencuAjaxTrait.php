ffe421d337233fd969ea04cb44e8c83c54c379f8
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-10-26 15:18:18 -0500

    Ajuste

--- app/Traits/Actaencu/ActaencuAjaxTrait.php
+++ app/Traits/Actaencu/ActaencuAjaxTrait.php
@@ -147,25 +147,4 @@ trait ActaencuAjaxTrait
         $respuest = $this->getTemacomboCT($dataxxxx)['comboxxx'];
         return response()->json($respuest);
     }
-
-    public function getTipoviaAjax(Request $request)
-    {
-        $dataxxxx=[
-            'temaxxxx' => 62,
-            'campoxxx' => 'nombre',
-            'orederby' => 'ASC',
-            'cabecera' => true,
-            'ajaxxxxx' => true,
-            'selected' => $request->selected,
-        ];
-        if($request->zonadire == 288 || $request->zonadire == 289){
-            $dataxxxx['inxxxxxx'] =[235];
-            $dataxxxx['cabecera'] =false;
-        }else {
-            $dataxxxx['notinxxx'] =[235];
-            $dataxxxx['cabecera'] =true;
-        }
-        $respuest = $this->getTemacomboCT($dataxxxx)['comboxxx'];
-        return response()->json($respuest);
-    }
 }
