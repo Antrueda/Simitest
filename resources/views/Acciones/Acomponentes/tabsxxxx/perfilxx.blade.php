462d50dda8d2082c99e2d1eff23614252c2afbb4
antrueda14 <antrueda14@gmail.com>
2021-10-27 09:56:52 -0500

    se corrigio upi reporte de evasión

--- resources/views/Acciones/Acomponentes/tabsxxxx/perfilxx.blade.php
+++ resources/views/Acciones/Acomponentes/tabsxxxx/perfilxx.blade.php
@@ -53,7 +53,7 @@
 
       <li class="list-group-item">
         <b>UPI</b>
-        <a class="float-right">{{ $todoxxxx['usuariox']->sis_nnaj->UpiPrincipal->nombre }}</a>
+        <a class="float-right">{{ $todoxxxx['usuariox']->sis_nnaj->UpiPrincipal->sis_depen->nombre }}</a>
       </li>
       <li class="list-group-item">
         <b>SERVICIO</b>
