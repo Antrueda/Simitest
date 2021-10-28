1c07402791b8dbc6c1f71ecebe006e8381c66efc
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-10-28 16:23:43 -0500

    Pasando valores a los formularios

--- app/Traits/AdmiActi/AdmiTiac/AdmiTiacVistasTrait.php
+++ app/Traits/AdmiActi/AdmiTiac/AdmiTiacVistasTrait.php
@@ -10,7 +10,14 @@ trait AdmiTiacVistasTrait
 {
     public function getVista( $dataxxxx)
     {
-        $this->opciones['estadoxx'] = SisEsta::combo(['cabecera' => false, 'esajaxxx' => false]);
+        $this->opciones['estadoxx'] = $this->getEstadosAECT([
+            'campoxxx' => 'id',
+            'orederby' => 'ASC',
+            'cabecera' => false,
+            'ajaxxxxx' => false,
+            'inxxxxxx' => [$this->estadoid],
+        ])['comboxxx'];
+        // $this->opciones['estadoxx'] = SisEsta::combo(['cabecera' => false, 'esajaxxx' => false]);
         $this->opciones['rutarchi'] = $this->opciones['rutacarp'] . 'Acomponentes.Acrud.' . $dataxxxx['accionxx'][0];
         $this->opciones['formular'] = $this->opciones['rutacarp'] . $this->opciones['carpetax'] . '.Formulario.' . $dataxxxx['accionxx'][1];
         $this->opciones['ruarchjs'] = [
@@ -29,6 +36,12 @@ trait AdmiTiacVistasTrait
             $this->pestania[0][2]=$this->opciones['parametr'];
             $this->getBotones(['crearxxx', [$this->opciones['routxxxx'].'.nuevoxxx', []], 2, 'NUEVO TIPO DE ACTIVIDAD', 'btn btn-sm btn-primary']);
         }
+        $this->opciones['motivoxx'] = $this->getEstusuariosAECT([
+            'cabecera' => true,
+            'ajaxxxxx' => false,
+            'estadoid' => $this->estadoid,
+            'formular' => 2687
+        ])['comboxxx'];
         $this->getPestanias($this->opciones);
         // Se arma el titulo de acuerdo al array opciones
         return view($this->opciones['rutacarp'] . 'pestanias', ['todoxxxx' => $this->opciones]);
