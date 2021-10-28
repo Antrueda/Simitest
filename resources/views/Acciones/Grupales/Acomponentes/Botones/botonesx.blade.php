f089851ec1391fb0238b2f683cde7dd8ddde1275
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-10-28 12:55:52 -0500

    Ajuste de botones ya que al eluminar el permiso de editar da error

--- resources/views/Acciones/Grupales/Acomponentes/Botones/botonesx.blade.php
+++ resources/views/Acciones/Grupales/Acomponentes/Botones/botonesx.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group card-footer text-muted text-center">
     @foreach ($todoxxxx['botoform'] as $botoform)
-        @if(!is_null($botoform['formhref']) && !is_null($botoform['routingx']))
+        @if(count($botoform) != 0)
             @switch($botoform['formhref'])
                 @case(1)
                     {{ Form::submit($botoform['tituloxx'], ['class'=>$botoform['clasexxx']]) }}
