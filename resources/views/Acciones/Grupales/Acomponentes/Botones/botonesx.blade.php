665f94849540c52a86ebbce15b0c043691f35863
David Sanchez <edwind.sanchez@idipron.gov.co>
2021-10-28 12:44:34 -0500

    Ajuste de botones ya que al eluminar el permiso de editar da error

--- resources/views/Acciones/Grupales/Acomponentes/Botones/botonesx.blade.php
+++ resources/views/Acciones/Grupales/Acomponentes/Botones/botonesx.blade.php
@@ -1,14 +1,15 @@
 <div class="form-group card-footer text-muted text-center">
     @foreach ($todoxxxx['botoform'] as $botoform)
-        @switch($botoform['formhref'] ?? 2)
-            @case(1)
-                {{ Form::submit($botoform['tituloxx'], ['class'=>$botoform['clasexxx']]) }}
-                @break
-            @case(2)
-                <a href="{{route($botoform['routingx'][0],$botoform['routingx'][1])}}"
-                class="{{ $botoform['clasexxx']}}">{{$botoform['tituloxx']}}</a>
-                @break
-        @endswitch
+        @if(!is_null($botoform) || count($botoform))
+            @switch($botoform['formhref'])
+                @case(1)
+                    {{ Form::submit($botoform['tituloxx'], ['class'=>$botoform['clasexxx']]) }}
+                    @break
+                @case(2)
+                    <a href="{{route($botoform['routingx'][0],$botoform['routingx'][1])}}"
+                    class="{{ $botoform['clasexxx']}}">{{$botoform['tituloxx']}}</a>
+                    @break
+            @endswitch
+        @endif
     @endforeach
-
 </div>
