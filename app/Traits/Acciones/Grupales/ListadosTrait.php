4bf44059a94b192ac040622014d4ffe62a0a3780
antrueda14 <antrueda14@gmail.com>
2021-11-23 12:51:20 -0500

    se realizo correciones en traslado para consulta de nnaj cedula

--- app/Traits/Acciones/Grupales/ListadosTrait.php
+++ app/Traits/Acciones/Grupales/ListadosTrait.php
@@ -706,15 +706,15 @@ trait ListadosTrait
             ])
                 ->join('sis_nnajs', 'traslado_nnajs.sis_nnaj_id', '=', 'sis_nnajs.id')
                 ->join('fi_datos_basicos', 'sis_nnajs.id', '=', 'fi_datos_basicos.sis_nnaj_id')
+                ->join('nnaj_nacimis', 'fi_datos_basicos.id', '=', 'nnaj_nacimis.fi_datos_basico_id')
+                ->join('nnaj_sexos', 'fi_datos_basicos.id', '=', 'nnaj_sexos.fi_datos_basico_id')
+                ->join('nnaj_docus', 'fi_datos_basicos.id', '=', 'nnaj_docus.fi_datos_basico_id')
                 ->join('traslados', 'traslado_nnajs.traslado_id', '=', 'traslados.id')
                 ->join('sis_estas', 'traslados.sis_esta_id', '=', 'sis_estas.id')
-                ->join('nnaj_docus', 'traslado_nnajs.sis_nnaj_id', '=', 'nnaj_docus.fi_datos_basico_id')
                 ->join('parametros as tipodocu', 'nnaj_docus.prm_tipodocu_id', '=', 'tipodocu.id')
-                ->join('nnaj_nacimis', 'traslado_nnajs.sis_nnaj_id', '=', 'nnaj_nacimis.fi_datos_basico_id')
-                ->join('nnaj_sexos', 'traslado_nnajs.sis_nnaj_id', '=', 'nnaj_sexos.fi_datos_basico_id')
                 ->where('traslado_nnajs.sis_esta_id', 1)
                 ->where('traslado_nnajs.traslado_id', $padrexxx->id);
-            return $this->getDtLT($dataxxxx, $request);
+            return $this->getDtras($dataxxxx, $request);
         }
     }
 
