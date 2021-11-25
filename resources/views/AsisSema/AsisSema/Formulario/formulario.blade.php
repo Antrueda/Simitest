b088a1f3fc20455f10ca97578bf64e0133449477
Edwin Daid Sanchez Balbin <e.david.sanchez.b@gmail.com>
2021-11-25 15:55:57 -0500

    Maquetado de formulario para la planilla de Asistencia Semanal

--- resources/views/AsisSema/AsisSema/Formulario/formulario.blade.php
+++ resources/views/AsisSema/AsisSema/Formulario/formulario.blade.php
@@ -1,7 +1,7 @@
 
 <div class="form-row">
     <div class="form-group col-md-4">
-        {!! Form::label('fechdili', 'fecha de diligeciamiento:', ['class' => 'control-label']) !!}
+        {!! Form::label('fechdili', '# de planilla:', ['class' => 'control-label']) !!}
         {!! Form::date('fechdili', null, ['class' => 'form-control form-control-sm', 'disabled']) !!}
         @if($errors->has('fechdili'))
         <div class="invalid-feedback d-block">
@@ -10,7 +10,7 @@
         @endif
     </div>
     <div class="form-group col-md-4">
-        {!! Form::label('sis_depen_id', 'UPI:', ['class' => 'control-label']) !!}
+        {!! Form::label('sis_depen_id', 'LUGAR DE INTERVENCIÓN, SEDE O DEPENDENCIA:', ['class' => 'control-label']) !!}
         {!! Form::select('sis_depen_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
         @if($errors->has('sis_depen_id'))
         <div class="invalid-feedback d-block">
@@ -19,7 +19,7 @@
         @endif
     </div>
     <div class="forn-group col-md-4">
-        {!! Form::label('sis_servicio_id', 'Servicio:', ['class' => 'control-labl']) !!}
+        {!! Form::label('sis_servicio_id', 'TIPO DE SERVICIO:', ['class' => 'control-labl']) !!}
         {!! Form::select('sis_servicio_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
         @if($errors->has('sis_servicio_id'))
         <div class="invalid-feedback d-block">
@@ -28,7 +28,7 @@
         @endif
     </div>
     <div class="form-group col-md-4">
-        {!! Form::label('sis_localidad_id', 'Localidad:', ['class' => 'control-label']) !!}
+        {!! Form::label('sis_localidad_id', 'Nombre del programa o actividad:', ['class' => 'control-label']) !!}
         {!! Form::select('sis_localidad_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
         @if($errors->has('sis_localidad_id'))
         <div class="invalid-feedback d-block">
@@ -37,7 +37,7 @@
         @endif
     </div>
     <div class="form-group col-md-4">
-        {!! Form::label('sis_upz_id', 'UPZ:', ['class' => 'control-label']) !!}
+        {!! Form::label('sis_upz_id', 'GRUPO Y GRADO:', ['class' => 'control-label']) !!}
         {!! Form::select('sis_upz_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
         @if($errors->has('sis_upz_id'))
         <div class="invalid-feedback d-block">
@@ -46,7 +46,7 @@
         @endif
     </div>
     <div class="form-group col-md-4">
-        {!! Form::label('sis_barrio_id', 'Barrio:', ['class' => 'control-label']) !!}
+        {!! Form::label('sis_barrio_id', 'Nombre del programa o actividad:', ['class' => 'control-label']) !!}
         {!! Form::select('sis_barrio_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
         @if($errors->has('sis_barrio_id'))
         <div class="invalid-feedback d-block">
@@ -55,16 +55,7 @@
         @endif
     </div>
     <div class="form-group col-md-6">
-        {!! Form::label('prm_accion_id', 'Acción:', ['class' => 'control-label']) !!}
-        {!! Form::select('prm_accion_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
-        @if($errors->has('prm_accion_id'))
-        <div class="invalid-feedback d-block">
-            {{ $errors->first('prm_accion_id') }}
-        </div>
-        @endif
-    </div>
-    <div class="form-group col-md-6">
-        {!! Form::label('prm_actividad_id', 'Actividad:', ['class' => 'control-label']) !!}
+        {!! Form::label('prm_actividad_id', 'CONVENIO /PROGRAMA:', ['class' => 'control-label']) !!}
         {!! Form::select('prm_actividad_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
         @if($errors->has('prm_actividad_id'))
         <div class="invalid-feedback d-block">
@@ -73,7 +64,7 @@
         @endif
     </div>
     <div class="form-group col-md-6">
-        {!! Form::label('objetivo', 'Objetivo:', ['class' => 'control-label']) !!}
+        {!! Form::label('objetivo', 'Actividad:', ['class' => 'control-label']) !!}
         {!! Form::textarea('objetivo', null, ['class' => 'form-control form-control-sm ']) !!}
         @if($errors->has('objetivo'))
         <div class="invalid-feedback d-block">
@@ -82,8 +73,17 @@
         @endif
     </div>
     <div class="form-group col-md-6">
-        {!! Form::label('desarrollo_actividad', 'Desarrollo de la actividad:', ['class' => 'control-label']) !!}
-        {!! Form::textarea('desarrollo_actividad', null, ['class' => 'form-control form-control-sm']) !!}
+        {!! Form::label('prm_accion_id', 'GRUPO:', ['class' => 'control-label']) !!}
+        {!! Form::select('prm_accion_id', [], null, ['class' => 'form-control form-control-sm select2']) !!}
+        @if($errors->has('prm_accion_id'))
+        <div class="invalid-feedback d-block">
+            {{ $errors->first('prm_accion_id') }}
+        </div>
+        @endif
+    </div>
+    <div class="form-group col-md-6">
+        {!! Form::label('desarrollo_actividad', 'Funcionario/Contratista que realiza el registro:', ['class' => 'control-label']) !!}
+        {!! Form::select('desarrollo_actividad', null, ['class' => 'form-control form-control-sm']) !!}
         @if($errors->has('desarrollo_actividad'))
         <div class="invalid-feedback d-block">
             {{ $errors->first('desarrollo_actividad') }}
@@ -91,15 +91,15 @@
         @endif
     </div>
     <div class="form-group col-md-6">
-        {!! Form::label('metodologia', 'Metodologia:', ['class' => 'control-label']) !!}
-        {!! Form::textarea('metodologia', null, ['class' => 'form-control form-control-sm']) !!}
+        {!! Form::label('metodologia', 'Responsable de UPI:', ['class' => 'control-label']) !!}
+        {!! Form::select('metodologia', null, ['class' => 'form-control form-control-sm']) !!}
         @if($errors->has('metodologia'))
         <div class="invalid-feedback d-block">
             {{ $errors->first('metodologia') }}
         </div>
         @endif
     </div>
-    <div class="form-group col-md-6">
+    {{-- <div class="form-group col-md-6">
         {!! Form::label('observaciones', 'Observaciones:', ['class' => 'control-label']) !!}
         {!! Form::textarea('observaciones', null, ['class' => 'form-control form-control-sm']) !!}
         @if($errors->has('observaciones'))
@@ -107,5 +107,5 @@
             {{ $errors->first('observaciones') }}
         </div>
         @endif
-    </div>
+    </div> --}}
 </div>
