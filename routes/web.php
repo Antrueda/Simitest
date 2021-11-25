97ed946bb7f24cc915c421c09c2c76ecf6bf7481
Edwin Daid Sanchez Balbin <e.david.sanchez.b@gmail.com>
2021-11-25 12:55:53 -0500

    Creando estructura base para la Planilla de Asistencia Semanal

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,7 @@ Route::group(['middleware' => ['auth', 'ChangePasswor', 'chequear.vinculacion']]
     });
     include_once('AdmiActi/web_adacmodu.php');
     include_once('Actaencu/web_actamodu.php');
+    include_once('AsisSema/web_asissemamodu.php');
 });
 
 
