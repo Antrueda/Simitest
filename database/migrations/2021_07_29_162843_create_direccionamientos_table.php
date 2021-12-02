a80701947e0065199159d9f723c55eecb833ad11
antrueda14 <antrueda14@gmail.com>
2021-12-02 16:36:20 -0500

    se creo la ruta de matricula cursos en acciones individuales y la administración correspondiente

--- database/migrations/2021_07_29_162843_create_direccionamientos_table.php
+++ database/migrations/2021_07_29_162843_create_direccionamientos_table.php
@@ -96,6 +96,70 @@ class CreateDireccionamientosTable extends Migration
         });
 
 
+        Schema::create('h_'.$this->tablaxxx, function (Blueprint $table) {
+            $table->increments('id')->start(1)->nocache();
+            $table->date('fecha')->nullable()->comment('FECHA QUE RETORNA EL NNA');
+            $table->integer('upi_id')->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->integer('area_id')->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->integer('tipo_id')->nullable()->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->string('s_primer_nombre')->comment('CAMPO PRIMER NOMBRE');
+            $table->string('s_segundo_nombre')->nullable()->comment('CAMPO SEGUNDO NOMBRE');
+            $table->string('s_primer_apellido')->comment('CAMPO PRIMER APELLIDO');
+            $table->string('s_segundo_apellido')->nullable()->comment('CAMPO SEGUNDO APELLIDO');
+            $table->string('s_nombre_identitario')->nullable()->comment('CAMPO DE NOMBRE IDENTITARIO');
+            $table->string('apodo')->nullable()->comment('CAMPO DE NOMBRE IDENTITARIO');
+            $table->string('s_documento')->comment('CAMPO NUMERO DE DOCUMENTO');
+            $table->date('d_nacimiento')->comment('CAMPO NUMERO DE DOCUMENTO');
+            $table = CamposMagicos::getForeign($table, 'prm_tipodocu_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'sis_pai');
+            $table = CamposMagicos::getForeign($table, 'sis_departam');
+            $table = CamposMagicos::getForeign($table, 'sis_municipio');
+            $table = CamposMagicos::getForeign($table, 'prm_sexo_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'prm_identidad_genero_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'prm_orientacion_sexual_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'prm_etnia_id', 'parametros');
+            $table = CamposMagicos::getForeignN($table, 'prm_poblacion_etnia_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'prm_cuentadisc_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'prm_condicion_id', 'parametros');
+            $table = CamposMagicos::getForeign($table, 'prm_cabeza_id', 'parametros');
+            $table->integer('prm_certifica_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->integer('prm_discapacidad_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->integer('departamento_cond_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->integer('municipio_cond_id')->unsigned()->nullable()->comment('CAMPO ID DE MUNICIPIO');
+            $table->integer('departamento_cert_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->integer('municipio_cert_id')->unsigned()->nullable()->comment('CAMPO ID DE MUNICIPIO');
+            $table->integer('prm_docuaco_id')->nullable()->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->string('primer_nombreaco')->nullable()->comment('CAMPO PRIMER NOMBRE ACOMPAÑANTE');
+            $table->string('segundo_nombreaco')->nullable()->comment('CAMPO SEGUNDO NOMBRE ACOMPAÑANTE');
+            $table->string('primer_apellidoaco')->nullable()->comment('CAMPO PRIMER APELLIDO ACOMPAÑANTE');
+            $table->string('segundo_apellidoaco')->nullable()->comment('CAMPO SEGUNDO APELLIDO ACOMPAÑANTE');
+            $table->string('documentoaco')->nullable()->comment('CAMPO NUMERO DE DOCUMENTO ACOMPAÑANTE');
+            $table->integer('userd_doc')->unsigned()->nullable()->comment('ID DE LA PERSONA RESPONSABLE');
+            $table->integer('userr_doc')->unsigned()->nullable()->comment('ID DE LA PERSONA RESPONSABLE');
+            $table->integer('sis_nnaj_id')->nullable()->unsigned()->comment('CAMPO ID NNAJ');
+            $table->integer('incompleto')->nullable()->unsigned()->comment('CAMPO ID NNAJ');
+            $table = CamposMagicos::h_magicos($table);
+        });
+
+        Schema::create('h_'.$this->tablaxxx2, function (Blueprint $table) {
+            $table->increments('id')->start(1)->nocache();
+            $table->integer('direc_id')->nullable()->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->longText('justificacion')->nullable()->comment('OBSERVACION DE LA SALIDA');
+            $table->integer('inter_id')->nullable()->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->integer('seguimiento_id')->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->integer('intra_id')->nullable()->unsigned()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->integer('sis_entidad_id')->nullable()->unsigned()->comment('ID DE LA ENTIDAD');
+            $table->integer('ent_servicio_id')->nullable()->unsigned()->comment('ID DE LA ENTIDAD');
+            $table->string('nombre_entidad')->nullable()->comment('CAMPO NOMBRE ENTIDAD');
+            $table->string('no_docinter')->nullable()->comment('CAMPO PARAMETRO DEPENDENCIA O UPI');
+            $table->string('nombreinter')->nullable()->comment('CAMPO PRIMER NOMBRE ACOMPAÑANTE');
+            $table->string('telefonointer')->nullable()->comment('CAMPO PRIMER NOMBRE ACOMPAÑANTE');
+            $table->string('intercargo')->nullable()->comment('CAMPO PRIMER NOMBRE ACOMPAÑANTE');
+            $table = CamposMagicos::getForeign($table, 'prm_tipoenti_id', 'parametros');
+            $table = CamposMagicos::h_magicos($table);
+        });
+
+
     }
 
     /**
@@ -107,6 +171,8 @@ class CreateDireccionamientosTable extends Migration
     {
         Schema::dropIfExists($this->tablaxxx2);
         Schema::dropIfExists($this->tablaxxx);
+        Schema::dropIfExists('h_' . $this->tablaxxx);
+        Schema::dropIfExists('h_' . $this->tablaxxx2);
 
     }
 }
