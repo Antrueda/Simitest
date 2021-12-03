1548f15a45c25fc1186e857775e4c0fe81378cfe
antrueda14 <antrueda14@gmail.com>
2021-12-02 23:30:20 -0500

    correcciones en migraciones de matricula cursos

---
+++ app/Models/Direccionamiento/Logs/HDireccionInst.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Direccionamiento;
+
+use App\Models\sistema\SisEntidad;
+use App\Models\sistema\SisServicio;
+use Illuminate\Database\Eloquent\Model;
+
+class HDireccionInst extends Model
+{
+    protected $table = 'h_direccion_inst'; 
+    protected $fillable = [
+        'direc_id', 'justificacion','sis_serv_id', 'seguimiento_id','intra_id',
+        'sis_entidad_id', 'ent_servicio_id','nombre_entidad', 'prm_tipoenti_id','inter_id', 
+        'no_docinter','nombreinter','telefonointer','intercargo', 'id_old', 'metodoxx', 'rutaxxxx', 'ipxxxxxx'
+   ];  //
+
+
+
+  
+
+}
--- database/migrations/2021_11_16_141734_create_curso_modulos_table.php
+++ database/migrations/2021_11_16_141734_create_curso_modulos_table.php
@@ -16,9 +16,18 @@ class CreateCursoModulosTable extends Migration
     {
         Schema::create('curso_modulos', function (Blueprint $table) {
             $table->increments('id')->start(1)->nocache();
-            $table = CamposMagicos::getForeign($table, 'cursos');
-            $table = CamposMagicos::getForeign($table, 'modulos');
+            $table->integer('cursos_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->foreign('cursos_id')->references('id')->on('cursos');
+            $table->integer('modulo_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->foreign('modulo_id')->references('id')->on('modulos');
             $table = CamposMagicos::magicos($table);
+        });
+
+        Schema::create('h_curso_modulos', function (Blueprint $table) {
+            $table->increments('id')->start(1)->nocache();
+            $table->integer('cursos_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table->integer('modulo_id')->unsigned()->nullable()->comment('CAMPO ID DE DEPARTAMENTO');
+            $table = CamposMagicos::h_magicos($table);
         });
     }
 
@@ -30,5 +39,6 @@ class CreateCursoModulosTable extends Migration
     public function down()
     {
         Schema::dropIfExists('curso_modulos');
+        Schema::dropIfExists('h_curso_modulos');
     }
 }
