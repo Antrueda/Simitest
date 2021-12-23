fc9d4656ff79fc3b6f87e5a61d3830c3149c0b5f
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-12-23 16:39:41 -0500

    se realia correccion a error que estaba presentando en la pesta violencia debido a que no contraba una variable

--- resources/views/Csd/Acomponentes/tabsxxxx/pestania.blade.php
+++ resources/views/Csd/Acomponentes/tabsxxxx/pestania.blade.php
@@ -1,7 +1,7 @@
 
 
 @canany([$permisox.'-leer', $permisox.'-crear', $permisox.'-editar'])
-    <?php $respuest = PCsd::getRDb(['padrexxx' => $todoxxxx['csdxxxxx'], 'pestania' => $pestania]); 
+    <?php $respuest = PCsd::getRDb(['padrexxx' => $todoxxxx['csdxxxxx'], 'pestania' => $pestania,'modeloxx'=>$todoxxxx['modeloxx']]); 
     ?>
     <li class="nav-item"><a class="nav-link{{ ($todoxxxx['slotxxxx']==$permisox) ?' active' : '' }}
     text-sm {{$respuest['disabled']}}" href="{{ $respuest['rutaxxxx'] }}">{{$tituloxx}}
