37ed0c6d0aaabab66288675d8ffe9b3cbb192865
antrueda14 <antrueda14@gmail.com>
2022-02-21 19:04:16 -0500

    Se agrego Matricula y matricula NNAJ

---
+++ app/Http/Requests/MatriculaAdmin/GrupoCrearRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\MatriculaAdmin;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GrupoCrearRequest extends FormRequest
+{
+    private $_mensaje;
+    private $_reglasx;
+
+    public function __construct()
+    {
+        $this->_mensaje = [
+            's_grupo.required' => 'El nombre es requerido',
+            's_grupo.max' => 'El nombre un máximo de 120 caracteres',
+            'estusuario_id.required'=> 'Seleccione la justificación de estado',
+        ];
+        $this->_reglasx = [
+            's_grupo' => 'required','string','max:120',
+            'prm_jornada' => 'required',
+            'estusuario_id' => 'required',
+            'dias' => 'required|array',
+        ];
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return $this->_mensaje;
+    }
+
+    /**
+     * Get the validation rules that Apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $this->validar();
+        return $this->_reglasx;
+    }
+
+    public function validar()
+    {
+        $dataxxxx = $this->toArray(); // todo lo que se envia del formulario
+    }
+}
--- app/Models/Acciones/Grupales/Educacion/GrupoMatricula.php
+++ app/Models/Acciones/Grupales/Educacion/GrupoMatricula.php
@@ -2,9 +2,53 @@
 
 namespace App\Models\Acciones\Grupales\Educacion;
 
+use App\Models\Educacion\Administ\Pruediag\EdaGrado;
+use App\Models\Parametro;
 use Illuminate\Database\Eloquent\Model;
 
 class GrupoMatricula extends Model
 {
-    //
+    protected $fillable = [
+        'user_crea_id', 'user_edita_id', 'sis_esta_id', 
+        's_grupo', 'observacion','horario','prm_jornada'
+    ];
+
+    public function prm_jornada(){
+        return $this->belongsTo(Parametro::class, 'prm_jornada');
+    }
+    public function dias(){
+      return $this->belongsToMany(Parametro::class,'grupo_dias', 'grupo_id', 'prm_dia_id');
+    }
+
+  public static function combo($cabecera, $ajaxxxxx)
+    {
+        $comboxxx = [];
+        if ($cabecera) {
+            if ($ajaxxxxx) {
+                $comboxxx[] = [
+                    'valuexxx' => '',
+                    'optionxx' => 'Seleccione'
+                ];
+            } else {
+                $comboxxx = [
+                    '' => 'Seleccione'
+                ];
+            }
+        }
+        $parametr = GrupoMatricula::select(['id as valuexxx', 's_grupo as optionxx'])
+            ->where('sis_esta_id', '1')
+            ->orderBy('s_grupo', 'desc')
+            ->get();
+        foreach ($parametr as $registro) {
+            if ($ajaxxxxx) {
+                $comboxxx[] = [
+                    'valuexxx' => $registro->valuexxx,
+                    'optionxx' => $registro->optionxx
+                ];
+            } else {
+                $comboxxx[$registro->valuexxx] = $registro->optionxx;
+            }
+        }
+        return $comboxxx;
+    }
 }
