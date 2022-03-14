39d5625e0ec0d8fd1d17be8bb228b579a7502523
johan <johanlozano839@gmail.com>
2022-03-14 10:54:12 -0500

    temacombo asistencia Diaria para el Tipo de Administacion item general

--- database/seeds/TemasTableSeeder.php
+++ database/seeds/TemasTableSeeder.php
@@ -408,6 +408,7 @@ class TemasTableSeeder extends Seeder
         Tema::create(['nombre' => 'GRUPOABC', 'user_crea_id' => '1', 'user_edita_id' => '1', 'sis_esta_id' => '1', 'created_at' => '2022-01-27T04:14:53.000000Z', 'updated_at' => '2022-01-27T04:14:53.000000Z',]); // 394
         Tema::create(['nombre' => 'NOVEDADES', 'user_crea_id' => '1', 'user_edita_id' => '1', 'sis_esta_id' => '1', 'created_at' => '2022-01-27T04:14:53.000000Z', 'updated_at' => '2022-01-27T04:14:53.000000Z',]); // 395
         Tema::create(['nombre' => 'ESTADO GESTIÓN MATRICULA', 'user_crea_id' => '1', 'user_edita_id' => '1', 'sis_esta_id' => '1', 'created_at' => '2022-01-27T04:14:53.000000Z', 'updated_at' => '2022-01-27T04:14:53.000000Z',]); // 396
+        Tema::create(['nombre' => 'ITEM GENERAL', 'user_crea_id' => '1', 'user_edita_id' => '1', 'sis_esta_id' => '1', 'created_at' => '2022-01-27T04:14:53.000000Z', 'updated_at' => '2022-01-27T04:14:53.000000Z',]); // 397
 
     }
 }
