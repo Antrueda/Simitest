aa898b0201c0124bbf8308cae97bff0f1c511a47
antrueda14 <antrueda14@gmail.com>
2022-04-25 16:23:11 -0500

    Se hizo merge conpruebas para gestion de matricula y cuestionario

--- resources/views/layouts/menus/educacion.blade.php
+++ resources/views/layouts/menus/educacion.blade.php
@@ -18,7 +18,7 @@
         @endcan
     </ul>
 
-    <ul class="nav nav-treeview">
+    {{-- <ul class="nav nav-treeview">
         @can('edaprudi-moduloxx')
                 <li class="nav-item">
                     <a href="{{ route('cgimodu-modulo') }}" class="nav-link">
@@ -27,7 +27,7 @@
                     </a>
                 </li>
             @endcan
-        </ul>
+        </ul> --}}
 
     <ul class="nav nav-treeview">
         @can('matriculaadmin-modulo')
@@ -40,7 +40,7 @@
             @endcan
         </ul>
         
-        {{-- <ul class="nav nav-treeview">
+        <ul class="nav nav-treeview">
             @can('cursosmodulosm-modulo')
                     <li class="nav-item">
                         <a href="{{ route('cursosmodulosm') }}" class="nav-link">
@@ -49,6 +49,6 @@
                         </a>
                     </li>
                 @endcan
-            </ul> --}}
+            </ul>
 
 </li>
