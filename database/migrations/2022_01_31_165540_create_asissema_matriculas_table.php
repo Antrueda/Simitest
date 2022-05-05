733f9c40340e81d09a463e644d1c038c5c67dc32
krmolinamolina <krmolinamolina@gmail.com>
2022-05-05 16:18:54 -0500

    h-mig

--- database/migrations/2022_01_31_165540_create_asissema_matriculas_table.php
+++ database/migrations/2022_01_31_165540_create_asissema_matriculas_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\CamposMagicos\CamposMagicos;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateAsissemaMatriculasTable extends Migration
 {
