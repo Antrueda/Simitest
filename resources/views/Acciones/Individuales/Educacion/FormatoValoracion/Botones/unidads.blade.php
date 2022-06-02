5cae8fc01547836d2a55d6e85f7a48e88909365f
antrueda14 <antrueda14@gmail.com>
2022-06-02 16:09:36 -0500

    Se creo una vista de las unidades en valoracion de competencias para ver su estado

--- resources/views/Acciones/Individuales/Educacion/FormatoValoracion/Botones/unidads.blade.php
+++ resources/views/Acciones/Individuales/Educacion/FormatoValoracion/Botones/unidads.blade.php
@@ -2,6 +2,7 @@
     @foreach(Tr::getUnidad(['padrexxx'=>$queryxxx->id]) as $dataxxxx)
       <tr>
         <td style="font-size: 10px;">{{ $dataxxxx->s_denominas}}</td>
+        <td style="font-size: 10px;">{{ $dataxxxx->concepto}}</td>
       </tr>
       @endforeach
 
