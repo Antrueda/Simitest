a097ea9b58440e029efe97f3ce77a1383d7ed8a9
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2022-07-06 20:59:40 -0500

    cambio

--- app/Models/Acciones/Grupales/Educacion/IMatricula.php
+++ app/Models/Acciones/Grupales/Educacion/IMatricula.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\DB;
 class IMatricula extends Model
 {
     protected $fillable = [
-        'fecha', 'prm_upi_id',
+        'fecha',
         'observaciones', 
         'user_doc1',
         'user_doc2',
