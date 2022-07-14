a9da4f5ba09b727f8dbb4025badb57118dfd6b95
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2022-07-14 13:41:35 -0500

    se realiza mejora para que al entrar por el editar se carguen los usuarios y la upi guardada en la tabla i_matriculas, ya que si la persona que entraba y no tienía la ipi o el rol no se cargaba la información correcta

--- app/Traits/Combos/CombosTrait.php
+++ app/Traits/Combos/CombosTrait.php
@@ -10,9 +10,6 @@ use App\Models\Educacion\Administ\Pruediag\EdaAsignatu;
 use App\Models\Educacion\Administ\Pruediag\EdaGrado;
 use App\Models\Educacion\Administ\Pruediag\EdaPresaber;
 use App\Models\Educacion\Usuariox\Pruediag\EduPresaber;
-use App\Models\Indicadores\InAccionGestion;
-use App\Models\Indicadores\InActsoporte;
-use App\Models\Indicadores\InLineabaseNnaj;
 use App\Models\Sistema\SisBarrio;
 use App\Models\sistema\SisDepartam;
 use App\Models\Sistema\SisDepen;
@@ -32,73 +29,7 @@ use Illuminate\Support\Facades\DB;
 
 trait CombosTrait
 {
-    public function getCampoCT($dataxxxx, $campoxxx)
-    {
-        if (!isset($dataxxxx['campoxxx'])) {
-            $dataxxxx['campoxxx'] = $campoxxx;
-        }
-        return $dataxxxx;
-    }
-
-    public function getDefaultCT($dataxxxx)
-    {
-        if (!isset($dataxxxx['orderxxx'])) {
-            $dataxxxx['orderxxx'] = 'ASC';
-        }
-        if (!isset($dataxxxx['cabecera'])) {
-            $dataxxxx['cabecera'] = true;
-        }
-        if (!isset($dataxxxx['ajaxxxxx'])) {
-            $dataxxxx['ajaxxxxx'] = false;
-        }
-        if (!isset($dataxxxx['selected'])) {
-            $dataxxxx['selected'] = [];
-        }
-        return $dataxxxx;
-    }
-
-    public function getCabecera($dataxxxx)
-    {
-        $comboxxx = [];
-        if ($dataxxxx['cabecera']) {
-            if ($dataxxxx['ajaxxxxx']) {
-                $comboxxx[] = ['valuexxx' => '', 'optionxx' => 'Seleccione'];
-            } else {
-                $comboxxx = ['' => 'Seleccione'];
-            }
-        }
-        return $comboxxx;
-    }
-
-    public function getCuerpoCombo($dataxxxx)
-    {
-        $comboxxx = $this->getCabecera($dataxxxx);
-        foreach ($dataxxxx['dataxxxx'] as $registro) {
-            if ($dataxxxx['ajaxxxxx']) {
-                $comboxxx[] = ['valuexxx' => $registro->valuexxx, 'optionxx' => strtoupper($registro->optionxx)];
-            } else {
-                $comboxxx[$registro->valuexxx] = strtoupper($registro->optionxx);
-            }
-        }
-        return $comboxxx;
-    }
-
-    public function getCuerpoUsuarioCT($dataxxxx)
-    {
-        $comboxxx = $this->getCabecera($dataxxxx);
-        foreach ($dataxxxx['dataxxxx'] as $registro) {
-            if ($dataxxxx['ajaxxxxx']) {
-                $selected = '';
-                if (in_array($registro->valuexxx, $dataxxxx['selected'])) {
-                    $selected = 'selected';
-                }
-                $comboxxx[] = ['valuexxx' => $registro->valuexxx, 'optionxx' => $registro->s_documento . ' - ' . strtoupper($registro->optionxx), 'selected' => $selected];
-            } else {
-                $comboxxx[$registro->valuexxx] = $registro->s_documento . ' - ' . strtoupper($registro->optionxx);
-            }
-        }
-        return $comboxxx;
-    }
+    use EstructuraComboTrait;
 
     public function getInRespuestas($dataxxxx)
     {
@@ -206,22 +137,7 @@ trait CombosTrait
         return $comboxxx;
     }
 
-    public function getCuerpoComboSinValueCT($dataxxxx)
-    {
-        $comboxxx = $this->getCabecera($dataxxxx);
-        foreach ($dataxxxx['dataxxxx'] as $registro) {
-            if ($dataxxxx['ajaxxxxx']) {
-                $selected = '';
-                if (in_array($registro->valuexxx, $dataxxxx['selected'])) {
-                    $selected = 'selected';
-                }
-                $comboxxx[] = ['valuexxx' => $registro->valuexxx, 'optionxx' => strtoupper($registro->optionxx), 'selected' => $selected];
-            } else {
-                $comboxxx[$registro->valuexxx] = strtoupper($registro->optionxx);
-            }
-        }
-        return $comboxxx;
-    }
+    
 
     public function getBarriosCT($dataxxxx)
     {
