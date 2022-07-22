131580dcadbf4d6ad7732b8922f89ace10b671de
antrueda14 <antrueda14@gmail.com>
2022-07-22 09:50:53 -0500

    Se agrego los request de salud

---
+++ app/Http/Requests/Acciones/Individuales/Salud/VsmedicinaCrearRequest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Requests\Acciones\Individuales\Salud;
+
+use App\Models\Acciones\Individuales\Educacion\MatriculaCursos\MatriculaCurso;
+use App\Models\fichaIngreso\FiDatosBasico;
+use Carbon\Carbon;
+use Illuminate\Foundation\Http\FormRequest;
+
+class VsmedicinaCrearRequest extends FormRequest
+{
+    private $_mensaje;
+    private $_reglasx;
+
+    public function __construct()
+    {
+
+        $this->_mensaje = [
+            'upi_id.required'=>'Seleccione la UPI de atención',
+            'upiorigen_id.required'=>'Seleccione la UPI de origen',
+            'fecha.required'=>'Ingrese la fecha de diligenciamiento',
+            'consul_id.required'=>'Seleccione la tipo de consulta',
+            'modal_id.required'=>'Seleccione la tipo de consulta',
+            'poblaci_id.required'=>'Seleccione el tipo de población',
+            'remico_id.required'=>'Seleccione el tipo de remisión Interinstitucional',
+            'entidad_id.required'=>'Seleccione la entidad de salud',
+            'afili_id.required'=>'Seleccione el estado de de afiliación de salud',
+            'remiesp_id.required_if'=>'Seleccione la especialidad',
+            'certifi_id.required'=>'Indique si requiere certificado',
+            'remiint_id.required'=>'Seleccione el tipo de remisión Intrainstitucional',
+
+            
+           
+        
+            ];
+        $this->_reglasx = [
+            'upi_id' => 'required',
+            'fecha' => 'required',
+            'upiorigen_id' => 'required',
+            'afili_id' => 'required',
+            'consul_id' => 'required',
+            'modal_id' => 'required',
+            'poblaci_id' => 'required',
+            'entidad_id' => 'required',
+            'afili_id' => 'required',
+            'remico_id' => 'required',
+            'remisal_id' => 'nullable',
+            'remigen_id' => 'nullable',
+            'recomenda' => 'nullable',
+            'motivoval' => 'nullable',
+            'certifi_id' => 'required',
+            'remiint_id' => 'required',
+            'remiesp_id' => 'required_if:remiint_id,1',
+            
+            
+           
+         
+           
+
+            ];
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return $this->_mensaje;
+    }
+    /**
+     * Get the validation rules that Apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $this->validar();
+        return $this->_reglasx;    }
+
+        public function validar()
+        {
+            $dataxxxx = $this->toArray(); // todo lo que se envia del formulario
+          
+         
+        }
+    }
+
+
+
+
+
+
