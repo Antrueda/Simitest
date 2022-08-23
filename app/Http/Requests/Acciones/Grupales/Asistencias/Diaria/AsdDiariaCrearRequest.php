25aaa31a2f070ca30582af96cc935ecef066159f
Johan Sebastian Martinez <55494581+johanlozanomartinez@users.noreply.github.com>
2022-08-23 09:55:46 -0500

    se sube a produccion  asistencia diaria- requests

--- app/Http/Requests/Acciones/Grupales/Asistencias/Diaria/AsdDiariaCrearRequest.php
+++ app/Http/Requests/Acciones/Grupales/Asistencias/Diaria/AsdDiariaCrearRequest.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Http\Requests\Acciones\Grupales\Asistencias\Diaria;
-
-
+use App\Traits\GestionTiempos\ManageTimeTrait;
+use App\Rules\TiempoCargueRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class AsdDiariaCrearRequest extends FormRequest
 {
     private $_mensaje;
     private $_reglasx;
+    use  ManageTimeTrait;
+
 
     public function __construct()
     {
@@ -66,6 +68,17 @@ class AsdDiariaCrearRequest extends FormRequest
      */
     public function rules()
     {
+        if ($this->fechdili != '' && $this->sis_depen_id ) {
+            $puedexxx = $this->getPuedeCargar([
+                'estoyenx' => 2, // 1 para acciones individuale y 2 para acciones grupales
+                'fechregi' => $this->fechdili,
+                'upixxxxx' => $this->sis_depen_id,
+                'formular'=>3,
+                ]);
+                $this->_reglasx['fechdili'] = new TiempoCargueRule([
+                    'puedexxx' => $puedexxx
+                ]);
+        }
         if ($this->prm_actividad_id == 2765) {
             $this->_reglasx['numepagi'] = ['required'];
         }
