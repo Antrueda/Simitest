285d690f086d21b4c6cc0e3fcf8962f6a7122285
hsneidersalgadoy <hsneider.salgado@idipron.gov.co>
2023-01-29 12:09:00 -0500

    roles y permisos

--- database/seeds/PermisosMedicinaMACSeeder.php
+++ database/seeds/PermisosMedicinaMACSeeder.php
@@ -22,5 +22,25 @@ class PermisosMedicinaMACSeeder extends Seeder
         $this->compleme = 'listar valoracion seguimiento medicina alternativa';
         $this->getBase();
 
+        $this->permisox = 'vsmacans';
+        $this->compleme = 'listar valoracion seguimiento medicina alternativa';
+        $this->getBase();
+
+        $this->permisox = 'vsmacant';
+        $this->compleme = 'listar valoracion seguimiento medicina alternativa';
+        $this->getBase();
+
+        $this->permisox = 'vsmaccon';
+        $this->compleme = 'listar valoracion seguimiento medicina alternativa';
+        $this->getBase();
+
+        $this->permisox = 'vsmacdia';
+        $this->compleme = 'listar valoracion seguimiento medicina alternativa';
+        $this->getBase();
+
+        $this->permisox = 'vsmedpma';
+        $this->compleme = 'listar valoracion seguimiento medicina alternativa';
+        $this->getBase();
+
     }
 }
