4ceba042e8c9244bb7f631106503747895ac9d84
krmolinamolina <krmolinamolina@gmail.com>
2023-01-31 08:37:59 -0500

    Cambios incidente #86 asistencia diaria y semanal nnaj de 6 a 18

--- resources/views/Acciones/Grupales/Asistencias/Semanal/Semanal/Formulario/editar.blade.php
+++ resources/views/Acciones/Grupales/Asistencias/Semanal/Semanal/Formulario/editar.blade.php
@@ -356,6 +356,9 @@
                 {{$todoxxxx['modeloxx']->userEdita->name}}
             </div>
         </div>
+        <div class="alert alert-info col-md-12" role="alert">
+            Recuerde que solo se pueden agregar NNAJ entre los 6 y 28 a&nacute;os, si lo busca y no lo encuentra es porque no cumple el criterio mencionado anteriormente
+        </div>
         <div class="col-md-12">
             @include($todoxxxx['rutacarp'].'Acomponentes.Acrud.index')
         </div>
